2d03cb633317dd8710d360dfd535f36bec774d5b
migbro <miguel.a.brown@gmail.com>
2020-01-16 16:12:52 -0500

    :tada: completed zscore script :hammer: reworked merge rsem :wrench: debugged 5c

--- scripts/6a_merge_rename_rsem.py
+++ scripts/6a_merge_rename_rsem.py
@@ -6,26 +6,23 @@ import os
 import concurrent.futures
 import gzip
 import json
+from get_file_metadata_helper import get_file_metadata
 
 
-def process_rsem_file(rsem_info, rsem_dir, fpkm_cts):
+def process_rsem_file(cbio_tum_id, rsem_dir, fpkm_cts):
     try:
-        rsem_path = rsem_dir + rsem_info[1]
-        bs_id = rsem_info[2]
+        rsem_path = file_meta_dict[cbio_dx][cbio_tum_id]['fname']
         if bs_id in mapping_dict:
-            samp_id = mapping_dict[bs_id]
-            samp_list.append(samp_id)
-            cur = gzip.open(rsem_dir + rsem_info[1])
-            next(cur)
-            for line in cur:
-                data = line.decode().rstrip('\n').split('\t')
-                gene_info = data[0].split('_')
-                ensg_id = gene_info[0]
-                fpkm_cts[ensg_id][samp_id] = data[-1]
-            cur.close()
-        else:
-            sys.stderr.write(rsem_path + ' not in mapping file, skipping!\n')
-            sys.stderr.flush()
+        samp_list.append(cbio_tum_id)
+        cur = gzip.open(rsem_dir + "/" + rsem_path)
+        next(cur)
+        for line in cur:
+            data = line.decode().rstrip('\n').split('\t')
+            gene_info = data[0].split('_')
+            ensg_id = gene_info[0]
+            fpkm_cts[ensg_id][cbio_tum_id] = data[-1]
+        cur.close()
+
     except Exception as e:
         sys.stderr.write('Error while processing ' + rsem_path + ', check that the file exists\n')
         print(e)
@@ -34,8 +31,8 @@ def process_rsem_file(rsem_info, rsem_dir, fpkm_cts):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Merge rsem files using cavatica file info.')
-    parser.add_argument('-f', '--rsem_file_list', action='store', dest='flist',
-                        help='File from step 1b, rsem file ids and file names')
+    parser.add_argument('-t', '--table', action='store', dest='table',
+                    help='Table with cbio project, kf bs ids, cbio IDs, and file names')
     parser.add_argument('-r', '--rsem-dir', action='store', dest='rsem_dir', help='rsem file directory')
     parser.add_argument('-j', '--config', action='store', dest='config_file', help='json config file with data types '
                                                                                    'and data locations')
@@ -52,12 +49,6 @@ if __name__ == "__main__":
         id_list.append(ens)
         gene_dict[ens] = sym
 
-    mapping_dict = {}
-    samp_list = []
-    for line in open(config_data['mapFileLoc']):
-        info = line.rstrip('\n').split('\t')
-        mapping_dict[info[1]] = info[0]
-    rsem_fh = open(args.flist)
     rsem_dir = args.rsem_dir
 
     if rsem_dir[-1] != '/':
@@ -67,34 +58,34 @@ if __name__ == "__main__":
         rsem_list.append(line.rstrip('\n').split('\t'))
     rsem_fh.close()
     
-
+    file_meta_dict = get_file_metadata(args.table, 'rsem')
     out_dir = 'merged_rsem/'
     try:
         os.mkdir(out_dir)
     except:
         sys.stderr.write('output dir already exists\n')
         sys.stderr.flush()
-    fpkm_cts = {}
-    for ens_id in id_list:
-        fpkm_cts[ens_id] = {}
-    # working with shared dict with "global" scope, no need to pass as param or may be overwritten
-
-    with concurrent.futures.ThreadPoolExecutor(config_data['threads']) as executor:
-        i = 1
-        n = 50
-        merge_results = {executor.submit(process_rsem_file, rsem_info, rsem_dir, fpkm_cts): rsem_info for rsem_info in rsem_list}
-        for processed in concurrent.futures.as_completed(merge_results):
-            if i % n == 0:
-                sys.stderr.write('Processed ' + str(i) + ' input files\n')
-            i += 1
-    samp_list.sort()
-    rsem_merge = open(out_dir + 'rsem_merged.txt', 'w')
-    rsem_merge.write('gene_id' + '\t' + 'gene_symbol' + '\t' + '\t'.join(samp_list) + '\n')
-    for ens_id in id_list:
-        rsem_merge.write(ens_id + '\t' + gene_dict[ens_id])
-        for samp_id in samp_list:
-            rsem_merge.write('\t' + fpkm_cts[ens_id][samp_id])
-        rsem_merge.write('\n')
-    rsem_merge.close()
+    for cbio_dx in file_meta_dict:
+        fpkm_cts = {}
+        for ens_id in id_list:
+            fpkm_cts[ens_id] = {}
+        # working with shared dict with "global" scope, no need to pass as param or may be overwritten
+        with concurrent.futures.ThreadPoolExecutor(config_data['threads']) as executor:
+            i = 1
+            n = 50
+            merge_results = {executor.submit(process_rsem_file, cbio_tum_id, rsem_dir, fpkm_cts): cbio_tum_id for cbio_tum_id in file_meta_dict[cbio_dx]}
+            for processed in concurrent.futures.as_completed(merge_results):
+                if i % n == 0:
+                    sys.stderr.write('Processed ' + str(i) + ' input files\n')
+                i += 1
+        samp_list.sort()
+        rsem_merge = open(out_dir + cbio_dx + '.rsem_merged.txt', 'w')
+        rsem_merge.write('gene_id' + '\t' + 'gene_symbol' + '\t' + '\t'.join(samp_list) + '\n')
+        for ens_id in id_list:
+            rsem_merge.write(ens_id + '\t' + gene_dict[ens_id])
+            for samp_id in samp_list:
+                rsem_merge.write('\t' + fpkm_cts[ens_id][samp_id])
+            rsem_merge.write('\n')
+        rsem_merge.close()
 
     sys.stderr.write('Done, check logs\n')
--- scripts/6b_z_score_transform.py
+++ scripts/6b_z_score_transform.py
@@ -5,11 +5,11 @@ import concurrent.futures
 import json
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 parser = argparse.ArgumentParser(description='Merge rsem files using cavatica file info.')
-parser.add_argument('-f', '--merged_rsem', action='store', dest='merged_rsem',
-                    help='File from step 6a, merged rsem file')
+parser.add_argument('-d', '--merged_rsem_dir', action='store', dest='merged_rsem_dir', help='merged rsem dir')
 parser.add_argument('-j', '--config', action='store', dest='config_file', help='json config file with data types '
                                                                                 'and data locations')
 args = parser.parse_args()
@@ -17,5 +17,19 @@ args = parser.parse_args()
 with open(args.config_file) as f:
     config_data = json.load(f)
 
-data = pd.read_csv(args.merged_rsem, sep="\t")
-data.set_index('Hugo_Symbol')
+flist = subprocess.check_output('find ' + args.merged_rsem_dir + ' -name *.rsem_merged.txt', shell=True)
+fname_list = flist.decode().split('\n')
+if fname_list[-1] == '':
+    fname_list.pop()
+for fname in fname_list:
+    data = pd.read_csv(fname, sep="\t", index_col=0)
+    samp_list = list(data.columns)[1:]
+    gene_list = list(data.index.values)
+    if len(samp_list) > 1:
+        new_fname = fname.replace('.rsem_merged.txt', '.rsem_merged_zscore.txt')
+        for gene in gene_list:
+            data[:gene] = stats.zscore(np.array(data[:gene]), axis=1)
+        data.to_csv(new_fname, sep='\t', mode='w', index=False)    
+    else:
+        sys.stderr.write('Only 1 sample found in ' + fname + ', skipping!\n')
+    
