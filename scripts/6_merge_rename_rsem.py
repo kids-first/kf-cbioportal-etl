a35715edbbfd859abf81d8878a34e098511c9a86
migbro <miguel.a.brown@gmail.com>
2020-01-23 21:22:52 -0500

    :wrench: debugged merge script, switch z score input to log2 + 1

--- scripts/6_merge_rename_rsem.py
+++ scripts/6_merge_rename_rsem.py
@@ -11,17 +11,6 @@ import numpy as np
 from scipy import stats
 
 
-def mt_process_master(rsem_file):
-    try:
-        current = pd.read_csv(rsem_dir + rsem_list[i], sep="\t", index_col=0)
-        sample = rna_subset.loc[rna_subset['File_Name'] == rsem_list[i], 'Cbio_Tumor_Name'].iloc[0]
-        sample_list.append(sample)
-        master_tbl = pd.concat([master_tbl, current[['FPKM']]], axis=1)
-    except Exception as e:
-        sys.stderr.write('Failed to concat ' + rsem_file + '\n')
-        sys.exit(1)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Merge rsem files using cavatica file info.')
     parser.add_argument('-t', '--table', action='store', dest='table',
@@ -30,6 +19,18 @@ if __name__ == "__main__":
     parser.add_argument('-j', '--config', action='store', dest='config_file', help='json config file with data types '
                                                                                    'and data locations')
     args = parser.parse_args()
+
+    def mt_collate_df(rsem_file):
+        try:
+            sample = rna_subset.loc[rna_subset['File_Name'] == rsem_file, 'Cbio_Tumor_Name'].iloc[0]
+            sample_list.append(sample)
+            current = pd.read_csv(rsem_dir + rsem_file, sep="\t", index_col=0)
+            cur_subset = current[['FPKM']]
+            cur_subset.rename(columns={"FPKM": sample}, inplace=True)
+            df_list.append(cur_subset)
+            return 1
+        except Exception as e:
+            sys.stderr.write(str(e) + "\nFailed to process " + rsem_file)
 
     with open(args.config_file) as f:
         config_data = json.load(f)
@@ -48,28 +49,30 @@ if __name__ == "__main__":
     
     # Use cbio table to create master table to choose represntative gene symbol, calc z scores, output both
     all_file_meta = pd.read_csv(args.table, sep="\t")
-    rna_subset = file_meta.loc[file_meta['File_Type'] == 'rsem']
-    rsem_list = rns_subset['File_Name'].to_list()
-    init_tbl = pd.read_csv(rsem_dir + rsem_list[0], sep="\t", index_col=0)
-    # Get cbio name to rename columns in mster table
+    rna_subset = all_file_meta.loc[all_file_meta['File_Type'] == 'rsem']
+    rsem_list = rna_subset['File_Name'].to_list()
+    # init_tbl = pd.read_csv(rsem_dir + rsem_list[0], sep="\t", index_col=0)
+    # Get cbio name to rename columns in master table
     sample_list = []
-    sample = rna_subset.loc[rna_subset['File_Name'] == rsem_list[0], 'Cbio_Tumor_Name'].iloc[0]
-    sample_list.append(sample)
-    master_tbl = init_tbl[['FPKM']]
-    # master_tbl.rename(columns={"FPKM": sample})
+    # master_tbl = init_tbl[['FPKM']]
+    # master_tbl.rename(columns={"FPKM": sample}, inplace=True)
     sys.stderr.write('Creating merged rsem table\n')
     x = 1
-    m = 100
+    m = 50
 
-    with concurrent.futures.ThreadPoolExecutor(config_data['threads']) as executor:
-        results = {executor.submit(mt_process_master, rsem_list[i],): rsem_list[i] for i in range(1, len(rsem_list), 1)}
+    df_list = []
+    with concurrent.futures.ThreadPoolExecutor(16) as executor:
+        results = {executor.submit(mt_collate_df, rsem_list[i]): rsem_list[i] for i in range(len(rsem_list))}
         for result in concurrent.futures.as_completed(results):
             if x % m == 0:
                 sys.stderr.write('Merged ' + str(x) + ' files\n')
                 sys.stderr.flush()
-    master_tbl.columns = sample_list
-    
+            x += 1
+    master_tbl = pd.concat(df_list, axis=1)
+    del df_list
+
     sys.stderr.write('Merge complete, picking representative gene transcripts\n')
+    sys.stderr.flush()
     master_tbl.reset_index(inplace=True)
     # drop ens_id_sym and replace with just sym
     gene_id_sym_list = master_tbl['gene_id'].to_list()
@@ -88,19 +91,27 @@ if __name__ == "__main__":
         to_dump = list(mean_expr.index)[1:]
         master_tbl.drop(to_dump, inplace=True)
     
-    sys.stderr.write('Calculating z scores\n')
     master_tbl.set_index('Hugo_Symbol', inplace=True)
     gene_sym_list = master_tbl.index
-    log_tf = np.log(np.array(master_tbl))
-    z_scored = stats.zscore(log_tf, axis = 1)
-    master_zscore_log = pd.DataFrame(z_scored, index=gene_sym_list, columns=sample_list)
-    master_zscore_log.fillna('NA', inplace=True)
-    
-    sys.stderr.write('Outputing project files\n')
     project_list = rna_subset.Cbio_project.unique()
+    
+    sys.stderr.write('Outputting FPKM expression results\n')
+    sys.stderr.flush()
     for project in project_list:
         sub_sample_list = list(rna_subset.loc[rna_subset['Cbio_project'] == project_list[0], 'Cbio_Tumor_Name'])
         expr_fname = out_dir + project + '.rsem_merged.txt'
         master_tbl[sub_sample_list].to_csv(expr_fname, sep='\t', mode='w', index=True)
+    
+    sys.stderr.write('Calculating z scores\n')
+    sys.stderr.flush()
+
+    z_scored = stats.zscore(np.log2(np.array(master_tbl + 1)), axis = 1)
+    del master_tbl
+    master_zscore_log = pd.DataFrame(z_scored, index=gene_sym_list, columns=sample_list)
+    
+    sys.stderr.write('Outputing z scored results\n')
+    
+    for project in project_list:
+        sub_sample_list = list(rna_subset.loc[rna_subset['Cbio_project'] == project_list[0], 'Cbio_Tumor_Name'])
         zscore_fname = out_dir + project + '.rsem_merged_zscore.txt'
         master_zscore_log[sub_sample_list].to_csv(zscore_fname, sep='\t', mode='w', index=True)
