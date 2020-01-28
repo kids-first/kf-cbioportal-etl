87d8c557b3b567eb62a67f41f276ce8ea3bdb583
migbro <miguel.a.brown@gmail.com>
2020-01-28 14:02:09 -0500

    :wrench: debugged script 8 :hammer: made helper work with multi studies, changed config desc

--- scripts/8_organize_upload_packages.py
+++ scripts/8_organize_upload_packages.py
@@ -18,49 +18,52 @@ args = parser.parse_args()
 def process_meta_study(meta_data, output_dir, study):
     meta_study = open(output_dir + "meta_study.txt", "w")
     meta_study.write("type_of_cancer: " + study + "\n")
-    meta_study.write("cancer_study_identifier: " + study + "\n")
     # default is dx value, otherwise make it "cancer_study_identifier" value from config, in not empty
     canc_study_id = study
     if meta_data["cancer_study_identifier"] != "":
         canc_study_id = meta_data["cancer_study_identifier"]
-    canc_study_id += + meta_data['dir_suffix']
-    meta_study.write("canc_study_id: " + canc_study_id + "\n")
+    canc_study_id += meta_data['dir_suffix']
+    meta_study.write("cancer_study_identifier: " + canc_study_id + "\n")
     name = dx_dict[study]
     if meta_data["name_append"] != "":
         name += " " + meta_data["name_append"]
     meta_study.write("name: " + name + "\n")
     desc = meta_data["description"][0]
     if len(meta_data["description"]) > 1:
-        desc += " " + name + " " + meta_data["description"][1]
-    meta_study.write("description: " + desc + "\nshort_name: " + canc_study_id + "\ngroups: " + meta_study["groups\n"])
+        desc += " " + dx_dict[study] + " " + meta_data["description"][1]
+    meta_study.write("description: " + desc + "\nshort_name: " + canc_study_id + "\ngroups: " + meta_data["groups"] + "\n")
     meta_study.close()
     return canc_study_id
 
 
 def process_meta_data(meta_data, output_dir, canc_study_id, study):
     for dtype in meta_data["dtypes"]:
-        cbio_name = meta_data["dtypes"]["cbio_name"]
+        # pointer for easier readability and dict key traciing
+        cur_data = meta_data["dtypes"][dtype]
+        cbio_name = cur_data["cbio_name"]
         parts = cbio_name.split("_")
-        meta_name = "meta_" + "_".join(parts[1])
+        meta_name = "meta_" + "_".join(parts[1:])
         meta_data_file = open(output_dir + meta_name, "w")
         meta_data_file.write("cancer_study_identifier: " + canc_study_id + "\n")
-        attr_dict = meta_data["dtypes"]["meta_file_attr"]
+        attr_dict = cur_data["meta_file_attr"]
         for mkey in attr_dict:
             meta_data_file.write(mkey + ": " + attr_dict[mkey] + "\n")
         meta_data_file.close()
         # create data_ links to data
-        cmd = "ln -s " + cwd + meta_data["dir"] + "/" + study + "." + meta_data["dtypes"]["ext"] + " " + output_dir + cbio_name
+        cmd = "ln -s " + cwd + meta_data["dir"] + "/" + study + "." + cur_data["ext"] + " " + output_dir + cbio_name
         subprocess.call(cmd, shell=True)
 
 
 def process_clinical_data(meta_data, output_dir, canc_study_id, study):
     for dtype in meta_data["dtypes"]:
-        cbio_name = meta_data["dtypes"]["cbio_name"]
+        # pointer for easier readability and dict key traciing
+        cur_data = meta_data["dtypes"][dtype]
+        cbio_name = cur_data["cbio_name"]
         parts = cbio_name.split("_")
-        meta_name = "meta_" + "_".join(parts[1])
+        meta_name = "meta_" + "_".join(parts[1:])
         meta_data_file = open(output_dir + meta_name, "w")
         meta_data_file.write("cancer_study_identifier: " + canc_study_id + "\n")
-        attr_dict = meta_data["dtypes"]["meta_file_attr"]
+        attr_dict = cur_data["meta_file_attr"]
         for mkey in attr_dict:
             meta_data_file.write(mkey + ": " + attr_dict[mkey] + "\n")
         meta_data_file.close()
@@ -69,19 +72,19 @@ def process_clinical_data(meta_data, output_dir, canc_study_id, study):
         subprocess.call(cmd, shell=True)
 
 
-def write_case_list(case_key, attr_dict, sample_list):
+def write_case_list(case_key, attr_dict, sample_list, case_dir):
     case_file = open(case_dir + case_key + ".txt", "w")
     case_file.write("cancer_study_identifier: " + canc_study_id + "\n")
     key_list = list(attr_dict)
     for i in range(0, len(key_list) - 1, 1):
-        cases_sequenced.write(key_list[i] + ": " + attr_dict[key_list[i]] + "\n")
-    cases_sequenced.write(key_list[-1] + ":" + attr_dict[key_list[-1]] + " (" + str(len(sample_list)) + ")\n")
+        case_file.write(key_list[i] + ": " + attr_dict[key_list[i]] + "\n")
+    case_file.write(key_list[-1] + ": " + attr_dict[key_list[-1]] + " (" + str(len(sample_list)) + ")\n")
     case_file.write("case_list_ids: " + "\t".join(sample_list) + "\n")
     case_file.close()
 
 def create_case_lists(data_dict, output_dir, canc_study_id, study):
+    case_dir = output_dir + "case_lists/"
     try:
-        case_dir = out_dir + "case_lists/"
         os.mkdir(case_dir)
     except:
         sys.stderr.write(case_dir + ' already exists.\n')
@@ -129,23 +132,21 @@ def create_case_lists(data_dict, output_dir, canc_study_id, study):
     muts_plus_fusion = muts_list + fusion_list
     muts_plus_fusion = [*{*muts_plus_fusion}]
     all_cases = muts_plus_fusion
-    write_case_list('cases_sequenced', config_data['cases_sequenced'], muts_plus_fusion)
+    write_case_list('cases_sequenced', config_data['cases_sequenced'], muts_plus_fusion, case_dir)
     if len(cna_list) > 0:
-        write_case_list('cases_cna', config_data['cases_cna'], cna_list)
+        write_case_list('cases_cna', config_data['cases_cna'], cna_list, case_dir)
         all_cases += cna_list
         muts_plus_cna = list(set(muts_list) & set(cna_list))
-        write_case_list('cases_cnaseq', config_data['cases_cnaseq'], cna_list)
+        write_case_list('cases_cnaseq', config_data['cases_cnaseq'], cna_list, case_dir)
     if len(rna_list) > 0:
-        write_case_list('cases_RNA_Seq_v2_mRNA', config_data['cases_RNA_Seq_v2_mRNA'], rna_list)
+        write_case_list('cases_RNA_Seq_v2_mRNA', config_data['cases_RNA_Seq_v2_mRNA'], rna_list, case_dir)
         all_cases += rna_list
         # loading mutations is a minimum, so if cna exists...3 way file can be made
         if len(cna_list) > 0:
             three_way = list(set(muts_list) & set(cna_list) & set(rna_list))
-            write_case_list('cases_3way_complete', config_data['cases_3way_complete'], three_way)
+            write_case_list('cases_3way_complete', config_data['cases_3way_complete'], three_way, case_dir)
     all_cases= [*{*muts_plus_fusion}]
-    write_case_list('cases_all', config_data['cases_all'], all_cases)
-            
-
+    write_case_list('cases_all', config_data['cases_all'], all_cases, case_dir)
 
 
 cwd = os.getcwd() + "/"
@@ -169,23 +170,35 @@ except:
     sys.stderr.write(out_dir + ' already exists.\n')
 
 for dx in dx_dict:
-    cur_dir = out_dir + dx
-    if config_data['study']['dir_suffix'] != "":
-        cur_dir += config_data['study']['dir_suffix']
-    cur_dir += "/"
     try:
-        os.mkdir(cur_dir)
-    except:
-        sys.stderr.write(cur_dir + ' already exists.\n')
-    sys.stderr.write("Creating meta study file for " + dx + "\n")
-    canc_study_id = process_meta_study(config_data['study'], cur_dir, dx)
-    data_keys = {"merged_mafs": 0, "merged_cnvs": 0, "merged_rsem": 0, "merged_fusion": 0}
-    for key in data_keys:
-        if config_data[key]["dir"] != "":
-            data_keys[key] = 1
-            process_meta_data(config_data[key], cur_dir, canc_study_id, dx)
-            sys.stderr.write("Creating meta data files and links for " + key + "\n")
+        if os.path.isdir(config_data["data_sheets"]["dir"] + "/" + dx):
+            cur_dir = ""
+            if config_data["study"]["cancer_study_identifier"] == "":
+                cur_dir = out_dir + dx
+                if config_data['study']['dir_suffix'] != "":
+                    cur_dir += config_data['study']['dir_suffix']
+            else:
+                cur_dir = out_dir + config_data["study"]["cancer_study_identifier"]
+            cur_dir += "/"
+            try:
+                os.mkdir(cur_dir)
+            except:
+                sys.stderr.write(cur_dir + ' already exists.\n')
+            sys.stderr.write("Creating meta study file for " + dx + "\n")
+            canc_study_id = process_meta_study(config_data['study'], cur_dir, dx)
+            data_keys = {"merged_mafs": 0, "merged_cnvs": 0, "merged_rsem": 0, "merged_fusion": 0}
+            for key in data_keys:
+                if config_data[key]["dir"] != "":
+                    data_keys[key] = 1
+                    process_meta_data(config_data[key], cur_dir, canc_study_id, dx)
+                    sys.stderr.write("Creating meta data files and links for " + key + "\n")
+                else:
+                    sys.stderr.write("Skipping meta files for " + key + "\n")
+            sys.stderr.write("Creating clinical meta sheets and links\n")
+            process_clinical_data(config_data["data_sheets"], cur_dir, canc_study_id, dx)
+            create_case_lists(data_keys, cur_dir, canc_study_id, dx)
         else:
-            sys.stderr.write("Skipping meta files for " + key + "\n")
-    sys.stderr.write("Creating clinical meta sheets and links\n")
-    process_clinical_data(config_data["data_sheets"], cur_dir, canc_study_id, dx)
+            sys.stderr.write("No datasheets for " + dx + ", skipping!\n")
+    except Exception as e:
+        sys.stderr.write(str(e) + "\nerror processing files for " + dx + "!\n")
+        pass
--- utilities/add_fusion_to_sample_sheets.py
+++ utilities/add_fusion_to_sample_sheets.py
@@ -1,30 +1,39 @@
 import sys
 import argparse
 import pandas as pd
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Add fusion flag to data clinical sample')
-    parser.add_argument('-f', '--fusion-file', action='store', dest='fusion_file',
-                    help='File with fusions')
-    parser.add_argument('-d', '--datasheet', action='store', dest='datasheet', help='cbio data sample sheet')
+    parser.add_argument('-f', '--fusion-dir', action='store', dest='fusion_dir',
+                    help='Dir with fusions')
+    parser.add_argument('-d', '--datasheet-list', action='store', dest='datasheet_list', help='cbio data sample sheet list')
+    parser.add_argument('-o', '--out-dir', action='store', dest='out_dir', help='output dir - don\'t make it the same as current datasheet location!!!')
 
     args = parser.parse_args()
-    fusion_data = pd.read_csv(args.fusion_file, sep="\t")
     added_header = ['With Fusion Data',
     'Indicator that sample has fusions associated with it',
     'STRING',
     '1',
     'WITH_FUSION_DATA']
-
-    sample_list = fusion_data.Tumor_Sample_Barcode.unique()
-    datasheet = open(args.datasheet)
-    for i in range(0, len(added_header)):
-        head = next(datasheet)
-        sys.stdout.write(head.rstrip('\n') + '\t' + added_header[i] + '\n')
-    for line in datasheet:
-        sys.stdout.write(line.rstrip('\n'))
-        data = line.split('\t')
-        if data[1] in sample_list:
-            sys.stdout.write('\tYES\n')
-        else:
-            sys.stdout.write('\tNO\n')
+    os.mkdir(args.out_dir)
+    datasheet_list = open(args.datasheet_list)
+    for fname in datasheet_list:
+        datasheet = open(fname.rstrip('\n'))
+        parts = fname.rstrip('\n').split("/")
+        fusion_data = pd.read_csv(args.fusion_dir + "/" + parts[-2] + ".fusions.txt", sep="\t")
+        sample_list = fusion_data.Tumor_Sample_Barcode.unique()
+        cur_dir = args.out_dir + "/" + parts[-2]
+        os.mkdir(cur_dir)
+        out = open(cur_dir + "/" + parts[-1], "w")
+        for i in range(0, len(added_header)):
+            head = next(datasheet)
+            out.write(head.rstrip('\n') + '\t' + added_header[i] + '\n')
+        for line in datasheet:
+            out.write(line.rstrip('\n'))
+            data = line.split('\t')
+            if data[1] in sample_list:
+                out.write('\tYES\n')
+            else:
+                out.write('\tNO\n')
+        out.close()
