f7e9c4a2ff3ebb81c56f3568780946cef86b273e
migbro <miguel.a.brown@gmail.com>
2020-02-13 18:51:33 -0500

    :hammer: added getting survival data to step 2 and incorprate into datasheets :wrench: fixed survival duration calculation

--- scripts/2_query_ds_by_bs_id.py
+++ scripts/2_query_ds_by_bs_id.py
@@ -6,6 +6,7 @@ import argparse
 import concurrent.futures
 import json
 from time import sleep
+import pdb
 
 
 def mt_query_calls(line):
@@ -14,10 +15,10 @@ def mt_query_calls(line):
         (tum_bs_id, norm_bs_id) = (info[0], info[1])
         norm_str = ''
         if norm_bs_id != 'NA' and norm_bs_id not in norm_seen:
-            bs_info = query_dataservice_bs_id(url, norm_bs_id, bs_attrs, pt_attrs, dx_attrs)
+            bs_info = query_dataservice_bs_id(url, norm_bs_id, bs_attrs, pt_attrs, dx_attrs, outcome_attrs)
             norm_str = norm_bs_id + '\t' + '\t'.join(bs_info) + '\n'
             norm_seen[norm_bs_id] = 1
-        bs_info = query_dataservice_bs_id(url, tum_bs_id, bs_attrs, pt_attrs, dx_attrs)
+        bs_info = query_dataservice_bs_id(url, tum_bs_id, bs_attrs, pt_attrs, dx_attrs, outcome_attrs)
         tum_str = tum_bs_id + '\t' + '\t'.join(bs_info) + '\n'
         return tum_str, norm_str
     except Exception as e:
@@ -25,7 +26,8 @@ def mt_query_calls(line):
         exit(1)
 
 
-def query_dataservice_bs_id(url, bs_id, bs_attrs, pt_attrs, dx_attrs):
+def query_dataservice_bs_id(url, bs_id, bs_attrs, pt_attrs, dx_attrs, outcome_attrs):
+
     bs_url = url + '/biospecimens/' + bs_id
     # sys.stderr.write(bs_url + '\n')
     bs_info = None
@@ -67,19 +69,24 @@ def query_dataservice_bs_id(url, bs_id, bs_attrs, pt_attrs, dx_attrs):
         sleep(1)
         pt_info = request('GET', url + pt_url)
     result.append(pt_info.json()['results']['kf_id'])
+    outcome_url = pt_info.json()['_links']['outcomes']
+    outcome_info = None
+    try:
+        outcome_info = request('GET', url + outcome_url)
+    except Exception as e:
+        sys.stderr.write('Got error ' + str(e) + ', could not retrieve info from ' + outcome_url + ', pausing and retrying\n')
+        sys.stderr.flush()
+        sleep(1)
+        outcome_info = request('GET', url + outcome_url)
     for attr in bs_attrs:
-        # sys.stderr.write(attr + ': ')
         res = bs_info.json()['results'][attr]
         if res is None:
             res = 'NULL'
-        # sys.stderr.write(res + '\n')
         result.append(str(res))
     for attr in pt_attrs:
-        # sys.stderr.write(attr + ': ')
         res = pt_info.json()['results'][attr]
         if res is None:
             res = 'NULL'
-        # sys.stderr.write(res + '\n')
         result.append(res)
     for attr in dx_attrs:
         dx_dict[attr] = []
@@ -90,7 +97,21 @@ def query_dataservice_bs_id(url, bs_id, bs_attrs, pt_attrs, dx_attrs):
                 else:   
                     dx_dict[attr].append(str(cur_res[attr]))
         result.append(';'.join(dx_dict[attr]))
-
+    # default to last outcome in list
+    try:
+        if len(outcome_info.json()['results']):
+            for attr in outcome_attrs:
+                res = outcome_info.json()['results'][-1][attr]
+                if res is None:
+                    res = 'NULL'
+                result.append(str(res))
+        else:
+            for attr in outcome_attrs:
+                res = 'NULL'
+                result.append(str(res))
+                sys.stderr.write("WARN: " + pt_info.json()['results']['kf_id'] + " has no outcome data\n")
+    except Exception as e:
+        sys.stderr.write(str(e) + "\n")
     return result
 
 
@@ -110,6 +131,7 @@ file_list = args.cav
 bs_attrs = ['external_aliquot_id', 'analyte_type', 'source_text_tissue_type', 'source_text_tumor_descriptor',
             'composition', 'external_sample_id']
 pt_attrs = ['external_id', 'gender', 'ethnicity', 'race']
+outcome_attrs = ['age_at_event_days', 'vital_status']
 dx_attrs = ['source_text_diagnosis', 'source_text_tumor_location', 'age_at_event_days']
 url = args.url
 
@@ -120,8 +142,9 @@ tum_out_fh.write('BS_ID\tPT_ID\t' + '\t'.join(bs_attrs) + '\t' + '\t'.join(pt_at
 if len(dx_attrs) > 0:
     norm_out_fh.write('\t' + '\t'.join(dx_attrs))
     tum_out_fh.write('\t' + '\t'.join(dx_attrs))
-norm_out_fh.write('\n')
-tum_out_fh.write('\n')
+# tack on outcome data, renaming the age field
+norm_out_fh.write('\toutcome_age_at_event_days\tvital_status\n')
+tum_out_fh.write('\toutcome_age_at_event_days\tvital_status\n')
 
 
 x = 1
@@ -140,6 +163,9 @@ with concurrent.futures.ThreadPoolExecutor(config_data['threads']) as executor:
         if norm_info != '':
             norm_out_fh.write(norm_info)
         x += 1
+# debug mode
+# for entry in task_fh:
+#     mt_query_calls(entry)
 norm_out_fh.close()
 tum_out_fh.close()
 sys.stderr.write('Done!')
