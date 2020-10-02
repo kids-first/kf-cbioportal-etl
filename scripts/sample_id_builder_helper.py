d980d84c0084c359a8cc2d09c83f4758d1b48683
migbro <miguel.a.brown@gmail.com>
2020-10-02 18:19:28 -0400

    :face_palm: actiually push commits listed from previous

--- scripts/sample_id_builder_helper.py
+++ scripts/sample_id_builder_helper.py
@@ -31,6 +31,19 @@ def build_samp_id(style, t_header, info):
         samp_id = info[sid_idx].split('.')[0] + '-' + info[ali_idx]
         return samp_id
 
+    elif style == 'pnoc_norm':
+        # sample ID will consist of kf external_sample_id with .SUFFIX removed and external_aliquot_id added
+        sid_idx = t_header.index('external_sample_id')
+        ali_idx = t_header.index('external_aliquot_id')
+        # Different styles for PNOC003 vs 008
+        # 008 style has naked 7316 ID, tack on aliquot to differentiate from tumor
+        samp_id = info[sid_idx] + '-' + info[ali_idx]
+
+        if info[sid_idx][-1] == "S":
+            # 003 style ending in .WGS or WXS
+            samp_id = info[sid_idx].split('.')[:-1][0]
+        return samp_id
+
     elif style == 'pnoc_rna_wes':
         # sample ID will consist of kf external_sample_id with .SUFFIX removed
         sid_idx = t_header.index('external_sample_id')
