9b39f47e983f78998db11c248a545fed93080a7c
Miguel Brown <migual.a.brown@gmail>
2021-06-10 14:25:49 +0000

    :wrench: fixed a bug in zscore calc

--- PUBLICATIONS/cptac_proteomics/zscore_protein.py
+++ PUBLICATIONS/cptac_proteomics/zscore_protein.py
@@ -11,18 +11,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Caculate z score of protein values.')
     parser.add_argument('-f', '--file', action='store', dest='in_file',
                     help='log2 transformed protein expression values')
+    parser.add_argument('-l', '--label', action='store', dest='label',
+                    help='ID column name')
+    parser.add_argument('-o', '--out-suffix', action='store',
+                    dest='out', help='Output filename desc')
 
     args = parser.parse_args()
     master_tbl = pd.read_csv(args.in_file, sep="\t")
     
-    master_tbl.set_index('Composite.Element.REF', inplace=True)
+    master_tbl.set_index(args.label, inplace=True)
     gene_sym_list = master_tbl.index
     sample_list=list(master_tbl.columns.values)
     sys.stderr.write('Calculating z scores\n')
     sys.stderr.flush()
 
-    z_scored = stats.zscore(master_tbl, axis = 1)
+    z_scored = stats.zscore(master_tbl, axis = 0)
     del master_tbl
     master_zscore_log = pd.DataFrame(z_scored, index=gene_sym_list, columns=sample_list)
     master_zscore_log.fillna(0, inplace=True)
-    master_zscore_log.to_csv("data_protein_quantification_Zscores.txt", sep='\t', mode='w', index=True)
+    master_zscore_log.to_csv("data_" + args.out + "_Zscores.txt", sep='\t', mode='w', index=True)
--- scripts/6_merge_rename_rsem.py
+++ scripts/6_merge_rename_rsem.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     sys.stderr.write('Calculating z scores\n')
     sys.stderr.flush()
 
-    z_scored = stats.zscore(np.log2(np.array(master_tbl + 1)), axis = 1)
+    z_scored = stats.zscore(np.log2(np.array(master_tbl + 1)), axis = 0)
     del master_tbl
     master_zscore_log = pd.DataFrame(z_scored, index=gene_sym_list, columns=sample_list)
     # this may be memory-intensive for some insane reson...
