0e181904952784c6b39da03a2fef30ea89ae95df
Miguel Brown <migual.a.brown@gmail>
2021-07-20 17:21:31 +0000

    :hammer: change header formation and added new fields to clinical files, per PR request from MSKCC :hammer: added back in genome version ro maf :wrench: revert axis thatz scoring is done

--- PUBLICATIONS/cptac_proteomics/zscore_log2_expr.py
+++ PUBLICATIONS/cptac_proteomics/zscore_log2_expr.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     sys.stderr.write('Calculating z scores\n')
     sys.stderr.flush()
 
-    z_scored = stats.zscore(master_tbl, axis = 1)
+    z_scored = stats.zscore(master_tbl, axis = 0)
     del master_tbl
     master_zscore_log = pd.DataFrame(z_scored, index=gene_sym_list, columns=sample_list)
     master_zscore_log.fillna(0, inplace=True)
-    master_zscore_log.to_csv("brain.zscore.log2_fpkm.tsv", sep='\t', mode='w', index=True)
+    master_zscore_log.to_csv("data_mrna_seq_v2_rsem_zscores_ref_all_samples.txt", sep='\t', mode='w', index=True)
