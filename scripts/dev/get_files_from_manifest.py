5e8e40ec0f97bda347cc374738d8fde66e556213
migbro <miguel.a.brown@gmail.com>
2021-07-27 11:10:18 -0400

    :hammer: finished manifest to download part

--- scripts/dev/get_files_from_manifest.py
+++ scripts/dev/get_files_from_manifest.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 
-import sys
 import argparse
-import concurrent.futures
-import re
-import pandas as pd
 import pdb
+import sys
+
+import pandas as pd
 
 parser = argparse.ArgumentParser(description='Get all files for a project.')
-parser.add_argument('-m', '--manifest', action='store', dest='manifest', help='download manifest file csv list')
-parser.add_argument('-w', '--wf-types', action='store', dest='wfs', help='csv list of workflow types to download')
+parser.add_argument('-m', '--manifest-list', action='store', dest='manifest', help='csv list of of genomic file location manifests')
+parser.add_argument('-f', '--file-types', action='store', dest='fts', help='csv list of workflow types to download')
 
 args = parser.parse_args()
-manifest = pd.read_csv(args.manifest)
+# concat multiple possible manifests
+sys.stderr.write("Concatenating manifests\n")
+manifest_list = args.manifest.split(",")
+manifest_concat = pd.DataFrame()
+for manifest in manifest_list:
+    sys.stderr.write("Processing " + manifest + "\n")
+    current = pd.read_csv(manifest)
+    if manifest_concat.empty:
+        manifest_concat = current.copy()
+    else:
+        manifest_concat = manifest_concat.append(current, ignore_index=True)
 
-wf_types = args.wfs.split(",")
-
-selected = manifest[manifest['Workflow_type']].isin(wf_types)
+file_types = args.fts.split(",")
+# subset concatenated manifests
+sys.stderr.write("Subsetting concatenated manifest\n")
+selected = manifest_concat[manifest_concat['File_type'].isin(file_types)]
 # remove vcfs as we only want mafs
 pattern_del = ".vcf.gz"
 filter = selected['File_name'].str.contains(pattern_del)
 selected = selected[~filter]
 
 out_file = "manifest_subset.tsv"
-selected.to_csv(expr_fname, sep='\t', mode='w', index=False)
+selected.to_csv(out_file, sep='\t', mode='w', index=False)
+
+
