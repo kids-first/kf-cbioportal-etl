a0668a3a8ceafe3e0c559668e6f8e002a8421bd4
Miguel Brown <migual.a.brown@gmail>
2021-07-27 19:36:31 +0000

    :hammer: Completed objectve of creating a download frm manifest script

--- scripts/dev/get_files_from_manifest.py
+++ scripts/dev/get_files_from_manifest.py
@@ -4,9 +4,34 @@ import argparse
 import pdb
 import sys
 import boto3
-from boto3.s3.transfer import TransferConfig
+import concurrent.futures
+import urllib3
 import os
 import pandas as pd
+
+
+def mt_type_download(file_type):
+    """
+    Download files from each desired file type at the same time
+    """
+    sys.stderr.write("Downloading " + file_type + " files\n")
+    sys.stderr.flush()
+    sub_file_list = list(selected.loc[selected['File_type'] == file_type, 'S3_path'])
+    try:
+        os.mkdir(file_type)
+    except Exception as e:
+        sys.stderr.write(str(e) + " error while making directory for " + file_type + "\n")
+    for loc in sub_file_list:
+        out = file_type + "/" + loc.split("/")[-1]
+        parse_url = urllib3.util.parse_url(loc)
+        try:
+            dl_client.download_file(Bucket=parse_url.host, Key=parse_url.path.lstrip("/"), Filename=out)
+        except Exception as e:
+            sys.stderr.write(str(e) + " could not download from " + loc + "\n")
+            sys.stderr.flush()
+    sys.stderr.write("Completed downloading files for " + file_type + "\n")
+    sys.stderr.flush()
+
 
 parser = argparse.ArgumentParser(description='Get all files for a project.')
 parser.add_argument('-m', '--manifest-list', action='store', dest='manifest', help='csv list of of genomic file location manifests')
@@ -16,6 +41,7 @@ parser.add_argument('-p', '--profile', action='store', dest='profile', help='aws
 args = parser.parse_args()
 # concat multiple possible manifests
 sys.stderr.write("Concatenating manifests\n")
+sys.stderr.flush()
 manifest_list = args.manifest.split(",")
 manifest_concat = pd.DataFrame()
 for manifest in manifest_list:
@@ -29,6 +55,7 @@ for manifest in manifest_list:
 file_types = args.fts.split(",")
 # subset concatenated manifests
 sys.stderr.write("Subsetting concatenated manifest\n")
+sys.stderr.flush()
 selected = manifest_concat[manifest_concat['File_type'].isin(file_types)]
 # remove vcfs as we only want mafs
 pattern_del = ".vcf.gz"
@@ -41,9 +68,8 @@ selected.to_csv(out_file, sep='\t', mode='w', index=False)
 # download files by type
 session = boto3.Session(profile_name=args.profile)
 dl_client = session.client('s3')
-pdb.set_trace()
-for ftype in file_types:
-    sub_file_list = list(selected.loc[selected['File_type'] == ftype, 'S3_path'])
-    os.mkdir(ftype)
-    for loc in sub_file_list:
-        out = ftype + "/" + loc.split("/")[-1]
+# pdb.set_trace()
+
+with concurrent.futures.ThreadPoolExecutor(16) as executor:
+    results = {executor.submit(mt_type_download, ftype): ftype for ftype in file_types}
+
