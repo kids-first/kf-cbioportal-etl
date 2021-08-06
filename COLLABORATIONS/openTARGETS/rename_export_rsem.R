08800f4cf64d7a4de7be580c06f460cca9251752
Miguel Brown <migual.a.brown@gmail>
2021-08-06 21:32:29 +0000

    :tada: added new subset script to help make proper sample names and proper cohort :hammer: updated transformation scripts to match updates

--- COLLABORATIONS/openTARGETS/rename_export_rsem.R
+++ COLLABORATIONS/openTARGETS/rename_export_rsem.R
@@ -1,16 +1,17 @@
 if(!require(data.table)){install.packages('data.table')}
 if(!require(readr)){install.packages('readr')}
-rna = readRDS("./gene-expression-rsem-tpm-collapsed.rds")
+rna = readRDS("../INPUTS/gene-expression-rsem-tpm-collapsed.rds")
 # Read in table with header: Cbio_project	T_CL_BS_ID	Sample Type	Cbio_Tumor_Name	File_Type
 # Example entry: brain	BS_6H1C1ME9	RNA	7316-2558_460366	rsem
 # should have only RNA names in it
-map_ids = read.csv(sep = "\t", header=TRUE, "../cbio_pbta_style_names.txt")
+map_ids = read.csv(sep = "\t", header=TRUE, "../INPUTS/cbio_pbta_style_names.txt")
 # For this load, subset only on pbta entries
 pbta_rna = rna[,which(colnames(rna) %in% map_ids$T_CL_BS_ID)]
 rownames(pbta_rna) = rownames(rna)
 # Rename BS IDs to cBio IDs
 setnames(pbta_rna, old=as.character(map_ids$T_CL_BS_ID), new=as.character(map_ids$Cbio_Tumor_Name))
 write_tsv(data.frame("Hugo_Symbol"=rownames(pbta_rna),pbta_rna, check.names = FALSE),"data_rna_seq_v2_mrna.txt", quote_escape="none")
-# Get z score of log2 tpm with added pseudocount
-pbta_zscore = t(scale(t(log2(pbta_rna + 1))))
+# Get z score of log2 tpm with added pseudocount - round to 4 places as added precision not needed
+pbta_zscore = round(t(scale(t(log2(pbta_rna + 1)))), 4)
 
+write_tsv(data.frame("Hugo_Symbol"=rownames(pbta_zscore),pbta_zscore, check.names = FALSE),"data_rna_seq_v2_mrna_median_Zscores.txt", quote_escape="none")
---
+++ COLLABORATIONS/openTARGETS/subset_histologies.py
@@ -0,0 +1,32 @@
+import argparse
+import pdb
+import sys
+import concurrent.futures
+import os
+import pandas as pd
+
+
+parser = argparse.ArgumentParser(description='Subset a hisologies file.')
+parser.add_argument('-f', '--histologies-file', action='store', dest='hist', help='histoligies file to subset')
+
+args = parser.parse_args()
+
+hist_file = pd.read_csv(args.hist, sep="\t", low_memory=False)
+#pdb.set_trace()
+cohorts = ["PBTA", "GMKF"]
+cgs = ["Neuroblastoma", "Acute Myeloid Leukemia"]
+# hist_subset1 = hist_file[hist_file['cohort'].isin(cohorts)]
+hist_subset1 = hist_file[hist_file['cohort'] == cohorts[0]]
+test1 = hist_file['cohort'] == "TARGET"
+test2 = hist_file['cancer_group'].isin(cgs)
+hist_subset2 = hist_file[test1 & test2]
+# fudge parent aliquot ID to be able to use next script
+
+hist_subset2 = hist_subset2.assign(parent_aliquot_id = hist_subset2['Kids_First_Participant_ID'] + "-" + hist_subset2['sample_id'] + "-" + cohorts[0])
+#pdb.set_trace()
+hist_subset3 = hist_file[hist_file['cohort'] == cohorts[1]]
+hist_subset3 = hist_subset3.assign(parent_aliquot_id = hist_subset3['Kids_First_Participant_ID'] + "-" + hist_subset3['sample_id'] + "-" + cohorts[1])
+
+hist_subset2 = hist_subset2.append(hist_subset3, ignore_index=True)
+hist_subset1.append(hist_subset2, ignore_index=True).to_csv("histologies_subset.tsv", sep='\t', mode='w', index=False)
+#hist_subset = hist_subset1.append(hist_subset2, ignore_index=True)
