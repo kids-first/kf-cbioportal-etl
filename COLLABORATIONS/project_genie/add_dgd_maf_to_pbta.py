1bc5f99c324532b3d6d3c3d90fd5423752b0bbdf
migbro <miguel.a.brown@gmail.com>
2021-08-31 18:18:15 +0000

    :construction: dev scripts to git new workflow

--- COLLABORATIONS/project_genie/add_dgd_maf_to_pbta.py
+++ COLLABORATIONS/project_genie/add_dgd_maf_to_pbta.py
@@ -6,9 +6,12 @@ import pdb
 parser = argparse.ArgumentParser(description='Output fields from maf file based on header - meant to be appended to an existing file!')
 parser.add_argument('-i', '--header', action='store', dest='header', help='File with maf header only')
 parser.add_argument('-m', '--maf-dir', action='store', dest='maf_dir', help='maf file directory')
+parser.add_argument('-t', '--table', action='store', dest='table',
+                    help='Table with cbio project, kf bs ids, cbio IDs, and file names')
 
 args = parser.parse_args()
-maf_list = os.listdir(args.maf_dir)
+file_meta_dict = get_file_metadata(args.table, 'DGD_MAF')
+
 header_file = open(args.header)
 skip = next(header_file)
 head = next(header_file)
@@ -18,23 +21,28 @@ header = head.rstrip('\n').split('\t')
 h_dict = {}
 for item in header:
     h_dict[item] = None
-for maf in maf_list:
-    sys.stderr.write("Processing " + maf + "\n")
-    cur = open(args.maf_dir + "/" + maf)
-    skip = next(cur)
-    m_head = next(cur)
-    m_header = m_head.rstrip('\n').split('\t')
-    for i in range(len(m_header)):
-        if m_header[i] in h_dict:
-            h_dict[m_header[i]] = i
-    # only print items in original headr and in same order, else print blank
-    for data in cur:
-        to_print = []
-        datum = data.rstrip('\n').split('\t')
-        for item in header:
-            if h_dict[item] != None:
-                to_print.append(datum[h_dict[item]])
-            else:
-                to_print.append("")
-        sys.stdout.write("\t".join(to_print) + "\n")
-    sys.stderr.write("Processed " + maf + "\n")
+for cbio_dx in file_meta_dict:
+    for cbio_tum_id in file_meta_dict[cbio_dx]:
+        maf_fn = file_meta_dict[cbio_dx][cbio_tum_id]['fname']
+        sys.stderr.write("Processing " + maf + "\n")
+        cur = open(args.maf_dir + "/" + maf)
+        skip = next(cur)
+        m_head = next(cur)
+        m_header = m_head.rstrip('\n').split('\t')
+        tid_idx = m_header.index('Tumor_Sample_Barcode')
+        for i in range(len(m_header)):
+            if m_header[i] in h_dict:
+                h_dict[m_header[i]] = i
+        # only print items in original header and in same order, else print blank
+        for data in cur:
+            to_print = []
+            datum = data.rstrip('\n').split('\t')
+            # Set tumor barcode to cBio ID
+            datum[tid_idx] = cbio_tum_id
+            for item in header:
+                if h_dict[item] != None:
+                    to_print.append(datum[h_dict[item]])
+                else:
+                    to_print.append("")
+            sys.stdout.write("\t".join(to_print) + "\n")
+        sys.stderr.write("Processed " + maf + "\n")
