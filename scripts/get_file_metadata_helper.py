621aa7bd15b6f2092ee0a39c20d78f9662a391f5
migbro <miguel.a.brown@gmail.com>
2021-09-28 11:32:28 -0400

    :ok_hand: implement changes suggested from PR :art: used black formatter to style code

--- scripts/get_file_metadata_helper.py
+++ scripts/get_file_metadata_helper.py
@@ -1,17 +1,17 @@
 def get_file_metadata(table, ftype):
     tbl_fh = open(table)
     head = next(tbl_fh)
-    header = head.rstrip('\n').split('\t')
-    cp_idx = header.index('Cbio_project')
-    ft_idx = header.index('File_Type')
-    fn_idx = header.index('File_Name')
-    ct_idx = header.index('Cbio_Tumor_Name')
-    cn_idx = header.index('Cbio_Matched_Normal_Name')
-    kt_idx = header.index('T_CL_BS_ID')
-    kn_idx = header.index('Norm_BS_ID')
+    header = head.rstrip("\n").split("\t")
+    cp_idx = header.index("Cbio_project")
+    ft_idx = header.index("File_Type")
+    fn_idx = header.index("File_Name")
+    ct_idx = header.index("Cbio_Tumor_Name")
+    cn_idx = header.index("Cbio_Matched_Normal_Name")
+    kt_idx = header.index("T_CL_BS_ID")
+    kn_idx = header.index("Norm_BS_ID")
     meta_dict = {}
     for line in tbl_fh:
-        info = line.rstrip('\n').split('\t')
+        info = line.rstrip("\n").split("\t")
         if info[ft_idx] == ftype:
             project = info[cp_idx]
             fname = info[fn_idx]
@@ -22,9 +22,9 @@ def get_file_metadata(table, ftype):
             if project not in meta_dict:
                 meta_dict[project] = {}
             meta_dict[project][cbio_tum_samp] = {}
-            meta_dict[project][cbio_tum_samp]['fname'] = fname
-            meta_dict[project][cbio_tum_samp]['cbio_norm_id'] = cbio_norm_samp
-            meta_dict[project][cbio_tum_samp]['kf_tum_id'] = kf_tum_samp
-            meta_dict[project][cbio_tum_samp]['kf_norm_id'] = kf_norm_samp
+            meta_dict[project][cbio_tum_samp]["fname"] = fname
+            meta_dict[project][cbio_tum_samp]["cbio_norm_id"] = cbio_norm_samp
+            meta_dict[project][cbio_tum_samp]["kf_tum_id"] = kf_tum_samp
+            meta_dict[project][cbio_tum_samp]["kf_norm_id"] = kf_norm_samp
     tbl_fh.close()
     return meta_dict
--- scripts/get_files_from_manifest.py
+++ scripts/get_files_from_manifest.py
@@ -15,16 +15,20 @@ def mt_type_download(file_type):
     """
     sys.stderr.write("Downloading " + file_type + " files\n")
     sys.stderr.flush()
-    sub_file_list = list(selected.loc[selected['file_type'] == file_type, 's3_path'])
+    sub_file_list = list(selected.loc[selected["file_type"] == file_type, "s3_path"])
     try:
         os.mkdir(file_type)
     except Exception as e:
-        sys.stderr.write(str(e) + " error while making directory for " + file_type + "\n")
+        sys.stderr.write(
+            str(e) + " error while making directory for " + file_type + "\n"
+        )
     for loc in sub_file_list:
         out = file_type + "/" + loc.split("/")[-1]
         parse_url = urllib3.util.parse_url(loc)
         try:
-            dl_client.download_file(Bucket=parse_url.host, Key=parse_url.path.lstrip("/"), Filename=out)
+            dl_client.download_file(
+                Bucket=parse_url.host, Key=parse_url.path.lstrip("/"), Filename=out
+            )
         except Exception as e:
             sys.stderr.write(str(e) + " could not download from " + loc + "\n")
             sys.stderr.flush()
@@ -32,10 +36,24 @@ def mt_type_download(file_type):
     sys.stderr.flush()
 
 
-parser = argparse.ArgumentParser(description='Get all files for a project.')
-parser.add_argument('-m', '--manifest-list', action='store', dest='manifest', help='csv list of of genomic file location manifests')
-parser.add_argument('-f', '--file-types', action='store', dest='fts', help='csv list of workflow types to download')
-parser.add_argument('-p', '--profile', action='store', dest='profile', help='aws profile name')
+parser = argparse.ArgumentParser(description="Get all files for a project.")
+parser.add_argument(
+    "-m",
+    "--manifest-list",
+    action="store",
+    dest="manifest",
+    help="csv list of of genomic file location manifests",
+)
+parser.add_argument(
+    "-f",
+    "--file-types",
+    action="store",
+    dest="fts",
+    help="csv list of workflow types to download",
+)
+parser.add_argument(
+    "-p", "--profile", action="store", dest="profile", help="aws profile name"
+)
 
 args = parser.parse_args()
 # concat multiple possible manifests
@@ -57,20 +75,19 @@ sys.stderr.write("Subsetting concatenated manifest\n")
 sys.stderr.flush()
 # change col names to lower case for input compatibility
 manifest_concat.columns = manifest_concat.columns.str.lower()
-selected = manifest_concat[manifest_concat['file_type'].isin(file_types)]
+selected = manifest_concat[manifest_concat["file_type"].isin(file_types)]
 # remove vcfs as we only want mafs
 pattern_del = ".vcf.gz"
-filter = selected['file_name'].str.contains(pattern_del)
+filter = selected["file_name"].str.contains(pattern_del)
 selected = selected[~filter]
 
 out_file = "manifest_subset.tsv"
-selected.to_csv(out_file, sep='\t', mode='w', index=False)
+selected.to_csv(out_file, sep="\t", mode="w", index=False)
 
 # download files by type
 session = boto3.Session(profile_name=args.profile)
-dl_client = session.client('s3')
+dl_client = session.client("s3")
 # pdb.set_trace()
 
 with concurrent.futures.ThreadPoolExecutor(16) as executor:
     results = {executor.submit(mt_type_download, ftype): ftype for ftype in file_types}
-
