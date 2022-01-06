9435c2f870b700ea97f1644d14f695a9912d6b9e
migbro <miguel.a.brown@gmail.com>
2022-01-06 14:40:43 -0500

    :pencil: updated readme to include new db wrapper script :hamm: completed get study metadata script

--- scripts/get_study_metadata.py
+++ scripts/get_study_metadata.py
@@ -30,6 +30,35 @@ def config(filename='database.ini', section='postgresql'):
     return db
 
 
+def generic_pull(db_cur, tbl_name):
+    """
+    Simple helper function for SELECT * database calls
+    """
+    if '.' not in tbl_name:
+        tbl_sql = sql.SQL('SELECT * FROM {};').format(sql.Identifier(tbl_name))
+    else:
+        (schema, table) = tbl_name.split('.')
+        tbl_sql = sql.SQL('SELECT * FROM {}.{};').format(sql.Identifier(schema), sql.Identifier(table))
+
+    db_cur.execute(tbl_sql)
+    rows = db_cur.fetchall()
+    colnames = [desc[0] for desc in db_cur.description]
+    return (rows, colnames)
+
+
+def generic_print(out_file, rows, colnames):
+    """
+    Simple helper function to print results to existing file handle as tsv
+    """
+    out_file.write("\t".join(colnames) + "\n")
+    for row in rows:
+        # convert None to empty str
+        new_row = [str(i or '') for i in row]
+        out_file.write("\t".join(new_row) + "\n")
+    out_file.close()
+    return 0
+
+
 def get_data_clinical(db_cur, config_dict, prefix):
     """
     Depending on the prefix of patient or sample, will pull from related tables,
@@ -40,12 +69,11 @@ def get_data_clinical(db_cur, config_dict, prefix):
         print("Please pass sample or patient for prefix.")
         exit(1)
     tbl_name = config_dict['database_pulls'][prefix + '_file']['table']
-    # get sample table contents
-    tbl_sql = "SELECT * FROM " + tbl_name
-    db_cur.execute(tbl_sql)
-    rows = db_cur.fetchall()
-    # get table header, and use to select file header
-    colnames = [desc[0] for desc in db_cur.description]
+
+    # get table contents
+    (rows, colnames) = generic_pull(db_cur, tbl_name)
+
+    # use table header from colnames, and use to select file header
     head_name = config_dict['database_pulls'][prefix + '_head']['table']
     # get sample table contents, have to split if format schema.table
     if '.' not in head_name:
@@ -54,15 +82,12 @@ def get_data_clinical(db_cur, config_dict, prefix):
         (schema, table) = head_name.split('.')
         head_sql = sql.SQL('SELECT {} FROM {}.{};').format(sql.SQL(',').join(map(sql.Identifier, colnames)), sql.Identifier(schema), sql.Identifier(table))
     db_cur.execute(head_sql)
-    sample_head = db_cur.fetchall()
+    head = db_cur.fetchall()
     # create output file and combine results for final product
     out_file = open(datasheet_dir + "/" + config_data['database_pulls'][prefix + '_file']['out_file'], 'w')
-    for row in sample_head:
+    for row in head:
         out_file.write("\t".join(row) + "\n")
-    out_file.write("\t".join(colnames) + "\n")
-    for row in rows:
-        out_file.write("\t".join(row) + "\n")
-    out_file.close()
+    generic_print(out_file, rows, colnames)
     return 0
 
 
@@ -70,6 +95,7 @@ def get_manifests(db_cur, config_dict):
     """
     This iterates through the manifests section of the database_pulls and grabs and outputs all listed file manifests for ec2 download
     """
+    # Just a pointer variable for ease of reading and coding
     manifests = config_dict['database_pulls']['manifests']
     for manifest in manifests:
         try:
@@ -84,12 +110,8 @@ def get_manifests(db_cur, config_dict):
             rows = db_cur.fetchall()
             colnames = [desc[0] for desc in db_cur.description]
             out_file = open(manifests[manifest]['out_file'], 'w')
-            out_file.write("\t".join(colnames) + '\n')
-            for row in rows:
-                # convert None to empty str
-                new_row = [str(i or '') for i in row]
-                out_file.write("\t".join(new_row) + "\n")
-            out_file.close()
+            generic_print(out_file, rows, colnames)
+
         except Exception as e:
             print(e)
             exit(1)
@@ -100,7 +122,7 @@ parser = argparse.ArgumentParser(description="Pull clinical data and genomics fi
 
 parser.add_argument("-d", "--db-ini", action="store", dest="db_ini", help="Database config file - formatting like aws or sbg creds")
 parser.add_argument("-p", "--profile", action="store", dest="profile", help="ini profile name", default="postgresql")
-parser.add_argument("-c", "--config", action="store", dest="config_file", help="json config file with meta information; see REFS/case_meta_config.json example",)
+parser.add_argument("-c", "--config", action="store", dest="config_file", help="json config file with meta information; see REFS/pbta_all_case_meta_config.json example",)
 
 args = parser.parse_args()
 # Load database login info
@@ -113,13 +135,9 @@ try:
     conn = psycopg2.connect(**params)
     cur = conn.cursor()
         
-    # execute a statement
-    print('PostgreSQL database version:')
-    cur.execute('SELECT version()')
+    # dict to track keys with specific database calls
+    special_keys = {"sample_head": 0, "sample_file": 0, "patient_head": 0, "patient_file": 0, "manifests": 0}
 
-    # display the PostgreSQL database server version
-    db_version = cur.fetchone()
-    print(db_version)
     try:
         os.mkdir(datasheet_dir)
     except Exception as e:
@@ -127,11 +145,21 @@ try:
     get_data_clinical(cur, config_data, 'sample')
     get_data_clinical(cur, config_data, 'patient')
     get_manifests(cur, config_data)
+
+    # For all other tables to be printed simply, not in special_keys
+    for key in config_data['database_pulls']:
+        if key not in special_keys and key != '_comment':
+            tbl_name = config_data['database_pulls'][key]['table']
+            (rows, colnames) = generic_pull(cur, tbl_name)
+            out_fn = config_data['database_pulls'][key]['out_file']
+            if key == 'gene_file':
+                out_fn = datasheet_dir + "/" + out_fn
+            out_file = open(out_fn, 'w')
+            generic_print(out_file, rows, colnames)
+
     # close the communication with the PostgreSQL
     cur.close()
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
     conn.close()
-
-
 conn.close()
