4a5fb47d92d522303abf8d556fa67e3e6ca0e85c
migbro <miguel.a.brown@gmail.com>
2022-01-06 11:00:35 -0500

    :tada: Created a python wrepper to get D3b warehoue files - WIP :pencil: updated config to support python wrapper and added in-file documentation

--- scripts/export_clinical.sh
+++ scripts/export_clinical.sh
@@ -10,7 +10,7 @@ psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path
 # PNOC Genomic files manifest
 psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO bix_workflows; COPY(select * from \"sd_8y99qzjj-genomics_file_manifest\" where file_type in ('RSEM_gene','annofuse_filtered_fusions_tsv','annotated_public_outputs','ctrlfreec_pval','ctrlfreec_info','ctrlfreec_bam_seg')) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > pnoc_genomics_file_manifest.txt
 # DGD Genomic files manifest
-psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO welshm3_dev_schema; COPY(select * from genomics_file_manifest where file_type in ('DGD_MAF', 'DGD_FUSION')) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > dgd_genomics_file_manifest.txt
+psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO welshm3_dev_schema; COPY(select biospecimen_id,trim(trailing from sample_type) as sample_type,participant_id,case_id,normal_id,tumor_id,family_id,file_name,trim(trailing from file_type) as file_type,trim(trailing from workflow_type) as workflow_type,trim(trailing from experiment_strategy) as experiment_strategy,volume_name,s3_path,aws_account,aws_role from genomics_file_manifest where file_type in ('DGD_MAF', 'DGD_FUSION')) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > dgd_genomics_file_manifest.txt
 # Genomics metadata file: often the -t arg input
 psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO prod_cbio; COPY(select * from pbta_all_genomics_etl_file) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > cbio_file_name_id.txt
 # Data clinical sample sheet - until a better function is written, need to output header lines first, then append data
@@ -22,4 +22,4 @@ psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path
 # Data gene matrix
 psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO prod_cbio; COPY(select * from pbta_all_data_gene_matrix) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > datasheets/data_gene_matrix_CHOP.txt
 # Sequencing Center Resource File
-psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO prod_cbio; COPY(select * from cbio_sq_info_etl_resource) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > seq_center_resource.txt
+psql -h d3b-warehouse-aurora-prd.d3b.io -U brownm28 postgres -c "SET search_path TO prod_cbio; COPY(select * from pbta_all_sq_info_etl_resource) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" > seq_center_resource.txt
