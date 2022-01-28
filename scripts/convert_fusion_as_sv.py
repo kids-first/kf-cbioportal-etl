cece33491cb85a1ddd4c50133e176d13db1a9fe9
migbro <miguel.a.brown@gmail.com>
2022-01-28 23:12:18 +0000

    :man_shrugging: Makes valid output, but cBio won't actually show on load page

--- scripts/convert_fusion_as_sv.py
+++ scripts/convert_fusion_as_sv.py
@@ -4,7 +4,8 @@ import sys
 import argparse
 import os
 import pandas as pd
-import numpy as np
+import pyranges
+import csv
 import pdb
 
 
@@ -43,20 +44,46 @@ if __name__ == "__main__":
         flist = rna_metadata.File_Name
         frame_list = []
         for i in range(0, len(flist), 1):
-            # concat annofuse file, rename Sample Column according to cBio name
-            pdb.set_trace()
-            ann_file = pd.read_csv(fusion_results + "/" + flist[i], sep="\t")
-            metadata_row = rna_metadata.loc[rna_metadata["File_Name"] == flist[i]]
-            ann_file = ann_file.assign(Samplle=metadata_row["Sample"])
-            frame_list.append(ann_file)
+            try:
+                # concat annofuse file, rename Sample Column according to cBio name
+                ann_file = pd.read_csv(fusion_results + "/" + flist[i], sep="\t")
+                ann_file = ann_file.assign(Sample=rna_metadata.at[i, "Cbio_Tumor_Name"])
+                frame_list.append(ann_file)
+            except Exception as e:
+                pdb.set_trace()
+                hold = 1
         concat_frame = pd.concat(frame_list)
         del frame_list
         fusion_data = concat_frame[
-            ["Gene1A", "Sample", "FusionName", "Caller", "Fusion_Type"]
+            [
+                "Sample",
+                "Gene1A",
+                "LeftBreakpoint",
+                "Gene1B",
+                "RightBreakpoint",
+                "Fusion_Type",
+                "JunctionReadCount",
+                "SpanningFragCount",
+                "annots",
+                "FusionName",
+                "Fusion_anno",
+                "Caller"
+                ]
         ]
         del concat_frame
         fusion_data = (
-            fusion_data.groupby(["Gene1A", "Sample", "FusionName", "Fusion_Type"])[
+            fusion_data.groupby([
+                "Sample",
+                "Gene1A",
+                "LeftBreakpoint",
+                "Gene1B",
+                "RightBreakpoint",
+                "Fusion_Type",
+                "JunctionReadCount",
+                "SpanningFragCount",
+                "annots",
+                "FusionName",
+                "Fusion_anno",])[
                 "Caller"
             ]
             .apply(", ".join)
@@ -77,77 +104,103 @@ if __name__ == "__main__":
     all_file_meta = pd.read_csv(args.table, sep="\t")
         
     rna_subset = all_file_meta.loc[all_file_meta["File_Type"] == r_ext]
-    rna_subset.rename(columns={"T_CL_BS_ID": "Sample"}, inplace=True)
-    rna_subset.set_index("Sample", inplace=True)
+    # reset index so that references work later while iterating
+    rna_subset = rna_subset.reset_index(drop=True)
     project_list = rna_subset.Cbio_project.unique()
-    cbio_master = init_cbio_master(args.fusion_results, args.mode, rna_subset)
+    cbio_master = init_cbio_master(args.fusion_results, rna_subset)
 
     # Get relevant columns
     cbio_master.set_index("Sample", inplace=True)
     # get cbio IDs and seq center
-    cbio_master = pd.merge(
-        cbio_master, rna_subset[["Cbio_Tumor_Name"]], on="Sample", how="inner"
-    )
+
     # drop bs ids
     cbio_master.reset_index(inplace=True)
-    cbio_master.drop("Sample", axis=1, inplace=True)
 
     cbio_master.rename(
         columns={
-            "Gene1A": "Hugo_Symbol",
-            "FusionName": "Fusion",
-            "Caller": "Method",
-            "Fusion_Type": "Frame",
-            "Cbio_Tumor_Name": "Tumor_Sample_Barcode",
-            "SQ_Value": "Center",
+            "Sample": "Sample_ID",
+            "Gene1A": "Site1_Hugo_Symbol",
+            "Gene1B": "Site2_Hugo_Symbol",
+            "Fusion_Type": "Site2_Effect_On_Frame",
+            "JunctionReadCount": "Tumor_Read_Count",
+            "SpanningFragCount": "Tumor_Split_Read_Count",
+            "annots": "Annotation",
+            "FusionName": "Event_Info",
+            "Fusion_anno": "External_Annotation",
+            "Caller": "Comments"
         },
-        inplace=True,
+        inplace=True
     )
+    # Fill in some defaults
+    cbio_master["Site1_Ensembl_Transcript_Id"] = ""
+    cbio_master["Site1_Entrez_Gene_Id"] = ""
+    cbio_master["Site1_Exon"] = ""
+    cbio_master["Site2_Ensembl_Transcript_Id"] = ""
+    cbio_master["Site2_Entrez_Gene_Id"] = ""
+    cbio_master["Site2_Exon"] = ""
+    cbio_master["Variant_Class"] = "Fusion"
+    cbio_master["DNA_support"] = "No"
+    cbio_master["RNA_support"] = "Yes"
+    cbio_master["NCBI_Build"] = "GRCh38"
+    cbio_master["Connection_Type"] = "5to3"
+    # Split some columns that have 2 cols worth of info
+    cbio_master[['Site1_Chromosome','Site1_Position']] = cbio_master.LeftBreakpoint.str.split(":", expand=True)
+    cbio_master[['Site2_Chromosome','Site2_Position']] = cbio_master.RightBreakpoint.str.split(":", expand=True)
+    cbio_master['Class'] = cbio_master.Annotation.str.split("],").str[1]
+    cbio_master['Annotation'] = cbio_master.Annotation.str.split("],").str[0]
+    # Reformat valiues to fit needs to be ALL CAPS, replace - with _, remove weird chars
+    cbio_master["Site2_Effect_On_Frame"] = cbio_master["Site2_Effect_On_Frame"].str.upper()
+    cbio_master['Site2_Effect_On_Frame'] = cbio_master['Site2_Effect_On_Frame'].str.replace('-','_')
+    cbio_master.loc[cbio_master["Annotation"] == "[", "Annotation"] = "NA"
+    cbio_master['Class'] = cbio_master['Class'].str.upper()
+    # Drop unneeded cols
+    cbio_master.drop(['LeftBreakpoint', 'RightBreakpoint'], axis=1, inplace=True)
 
-    cbio_master["DNA_support"] = "no"
-    cbio_master["RNA_support"] = "yes"
-    # create blank entrez ID column so that 3' gene names can be searched
-    cbio_master["Entrez_Gene_Id"] = ""
     # Reorder table
     order_list = [
         "Sample_ID",
         "Site1_Hugo_Symbol",
         "Site1_Entrez_Gene_Id",
+        "Site1_Ensembl_Transcript_Id",
+        "Site1_Exon",
         "Site1_Chromosome",
         "Site1_Position",
         "Site2_Hugo_Symbol",
         "Site2_Entrez_Gene_Id",
+        "Site2_Ensembl_Transcript_Id",
+        "Site2_Exon",
         "Site2_Chromosome",
         "Site2_Position",
         "Site2_Effect_On_Frame",
         "NCBI_Build",
+        "DNA_support",
+        "RNA_support",
         "Tumor_Read_Count",
         "Tumor_Split_Read_Count",
         "Annotation",
-        "Connection_Type",
+        "Connection_Type",cd data
         "Event_Info",
         "Class",
         "External_Annotation"
     ]
     cbio_master = cbio_master[order_list]
-    cbio_master.set_index("Hugo_Symbol", inplace=True)
+    # cbio_master.set_index("Sample_ID", inplace=True)
     for project in project_list:
         sub_sample_list = list(
             rna_subset.loc[rna_subset["Cbio_project"] == project, "Cbio_Tumor_Name"]
         )
         fus_fname = out_dir + project + ".fusions.txt"
-        fus_file = open(fus_fname, "w")
-        fus_tbl = cbio_master[cbio_master.Tumor_Sample_Barcode.isin(sub_sample_list)]
-        fus_tbl.reset_index(inplace=True)
+        fus_tbl = cbio_master[cbio_master.Sample_ID.isin(sub_sample_list)]
         fus_tbl.fillna("NA", inplace=True)
-        fus_head = "\t".join(fus_tbl.columns) + "\n"
-        fus_file.write(fus_head)
-        fus_data = list(fus_tbl.values)
-        # "hack" to allow 3' end of fusion to be searched
-        for data in fus_data:
-            fus_file.write("\t".join(data) + "\n")
-            (a, b) = data[4].split("--")
-            if a != b:
-                data[0] = b
-                fus_file.write("\t".join(data) + "\n")
-        fus_file.close()
+        fus_tbl.set_index("Sample_ID", inplace=True)
+        # fus_head = "\t".join(fus_tbl.columns) + "\n"
+        # fus_file.write(fus_head)
+        # fus_data = list(fus_tbl.values)
+        # # "hack" to allow 3' end of fusion to be searched
+        # for data in fus_data:
+        #     fus_file.write("\t".join(data) + "\n")
+        #     (a, b) = data[4].split("--")
+        #     if a != b:
+        #         data[0] = b
+        #         fus_file.write("\t".join(data) + "\n")
+        fus_tbl.to_csv(fus_fname, sep="\t", mode="w", index=True, quoting=csv.QUOTE_NONE)
