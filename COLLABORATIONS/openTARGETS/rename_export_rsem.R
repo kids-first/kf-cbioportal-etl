3a80b383a7f7627ff6e416d6875a1fb4fcdb9d5e
migbro <miguel.a.brown@gmail.com>
2022-07-26 18:06:33 +0000

    :pencil: updated description of input file

--- COLLABORATIONS/openTARGETS/rename_export_rsem.R
+++ COLLABORATIONS/openTARGETS/rename_export_rsem.R
@@ -14,7 +14,7 @@ option_list <- list(
   make_option(
     opt_str = "--map_id",
     type = "character",
-    help = "mapping ID file with headers: Cbio_project	T_CL_BS_ID	Sample Type	Cbio_Tumor_Name	File_Type"
+    help = "mapping ID file with headers: BS_ID	Sample Type	Cbio ID"
   ),
   make_option(
     opt_str = "--type",
@@ -27,7 +27,7 @@ opts <- parse_args(OptionParser(option_list = option_list))
 message("Reading in rds file ", opts$rna_rds)
 rna = readRDS(opts$rna_rds)
 
-# Example entry: brain	BS_6H1C1ME9	RNA	7316-2558_460366	rsem
+# Example entry: brain	BS_6H1C1ME9	RNA	7316-2558_460366
 message("Reading in sample naming file ", opts$map_id)
 map_ids = read.csv(sep = "\t", header=TRUE, opts$map_id)
 map_ids = map_ids[map_ids$Sample.Type == "RNA",]
@@ -36,7 +36,7 @@ subset_rna = rna[,which(colnames(rna) %in% map_ids$BS_ID)]
 rownames(subset_rna) = rownames(rna)
 # Rename BS IDs to cBio IDs
 message("Renaming samples and outputting tpm file")
-setnames(subset_rna, old=as.character(map_ids$BS_ID), new=as.character(map_ids$Cbio.ID))
+setnames(subset_rna, old=as.character(map_ids$BS_ID), new=as.character(map_ids$Cbio.ID), skip_absent=TRUE)
 
 write_tsv(data.frame("Hugo_Symbol"=rownames(subset_rna),subset_rna, check.names = FALSE),paste(opts$type, ".rsem_merged.txt", sep=""), escape="none")
 # Get z score of log2 tpm with added pseudocount - round to 4 places as added precision not needed
