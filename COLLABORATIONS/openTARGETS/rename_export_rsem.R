61643d4c3edd67a71fd997b46c13cd274f707804
migbro <miguel.a.brown@gmail.com>
2022-11-01 21:50:50 +0000

    Add zscore c++ implementation for speedup and memory optimization

--- COLLABORATIONS/openTARGETS/rename_export_rsem.R
+++ COLLABORATIONS/openTARGETS/rename_export_rsem.R
@@ -1,8 +1,11 @@
 if(!require(data.table)){install.packages('data.table')}
 if(!require(readr)){install.packages('readr')}
 if(!require(optparse)){install.packages('optparse')}
+if(!require(Rcpp)){install.packages('Rcpp')}
 library("optparse")
 library("data.table")
+library("Rcpp")
+sourceCpp('/home/ubuntu/tools/kf-cbioportal-etl/utilities/compute_zscore.cpp') #set path to the cpp file
 
 #process inputs
 option_list <- list(
@@ -42,57 +45,14 @@ write_tsv(data.frame("Hugo_Symbol"=rownames(subset_rna),subset_rna, check.names 
 # Get z score of log2 tpm with added pseudocount - round to 4 places as added precision not needed
 
 rm(rna)
-rm(opts)
 rm(map_ids)
 
 message("Calculating z scores of log2 tmp + 1")
+subset_zscore=compute_write_zscore(data.matrix(subset_rna),8) #c++ function to compute zscores with 8 threads
 #subset_zscore = round(t(scale(t(log2(subset_rna + 1)))), 4)
-genes=rownames(subset_rna)
 
-library("Rcpp")
-cppFunction("
-List compute_zscore(List num) {
-int n = num.size();
-float a =0;
-float sum =0;
-float st = 0;
-float var = 0;
-float mean =0;
-for(int i = 0; i < n; ++i) {
-  a=num[i];
-  a = std::log2(a + 1);
-  sum=sum+a;
-  num[i]=a;
-} 
-mean=sum/n;
-for(int i =0; i < n; ++i){
-  a=num[i];
-  var=var+(a-mean)*(a-mean);
-}
-st=std::sqrt(var/(n-1));
-int round_off = 10000;
-for(int i = 0; i < n; ++i) {
-    a=num[i];
-    a = std::ceil((a - mean)*round_off/st) ;
-    num[i]=a/round_off;
-}  
-return num;
-}")
-zscore <- list()
-test = t(subset_rna)
-message(test[0])
-stop()
-#a=1
-#message(Sys.time())
-for (x in genes){
-#message((a))
-#subset_zscore=compute_zscore(subset_rna[x,]) # less than 3 mins for 500 genes C++ function
-subset_zscore = round(scale(log2(subset_rna[x,] + 1)), 4) # less than 10 mins for 85 genes using scale function
-#zscore <-append(zscore, subset_zscore)
-#a=a+1
-}
-subset_zscore = t(subset_zscore)
-
-stop("Stopping msg")
-message("Outputting z scores")
-write_tsv(data.frame("Hugo_Symbol"=rownames(subset_zscore),subset_zscore, check.names = FALSE),paste(opts$type, ".rsem_merged_zscore.txt", sep=""), escape="none")
+rm(subset_rna)
+output_file_name=paste(opts$type, ".rsem_merged_zscore.txt", sep="")
+message("Writing zscore to a file")
+write_file(subset_zscore,output_file_name) #function in C++ to write matrix into tsv format with name of the file as output_file_name
+#write_tsv(data.frame("Hugo_Symbol"=rownames(subset_zscore),subset_zscore, check.names = FALSE),output_file_name, escape="none")
---
+++ utilities/compute_zscore.cpp
@@ -0,0 +1,124 @@
+//compute_zscore.cpp
+#include <Rcpp.h>
+#include <fstream>
+#include <thread>
+#include <unistd.h>
+using namespace Rcpp;
+
+NumericMatrix compute_write_zscore(NumericMatrix num, int no_threads) ;
+void write_file(NumericMatrix num,std::string output_file_name);
+void compute_zscore_row(NumericMatrix::Row row);
+
+//Function to write files in tsv format
+// [[Rcpp::export]]
+void write_file(NumericMatrix num,std::string output_file_name) 
+{
+    //Rcout<<"Writing z-scores using C++ Implementation"<<"\n";
+    std::ofstream myfile;
+    myfile.open(output_file_name);
+    CharacterVector ch = colnames(num);
+    CharacterVector ch1 = rownames(num);
+    myfile <<"Hugo_Symbol"<<"\t";
+    for(auto p = ch.begin(); p != ch.end(); ++p){
+          myfile << *p<<"\t";
+      }
+    myfile <<"\n";
+    for(int i =0; i < num.nrow();++i){
+        myfile << ch1[i]<<"\t";
+        for(int j =0; j <  num.ncol();++j ){
+            myfile << num(i,j)<<"\t";
+          }
+        myfile <<"\n";
+      }
+    myfile.close();
+}
+// Function to compute zscore
+void compute_zscore_row(NumericMatrix::Row row)
+{
+    float mean =0.0;
+    float sd = 0.0;
+    float var = 0.0;
+    int row_size=row.size();
+    for (auto i = row.begin(); i != row.end(); ++i){ // compute mean
+        if ( *i != 0)
+              *i = std::log2(*i+1);
+        mean=mean + *i ;      
+    }
+    mean=mean/row_size;
+    for (auto i = row.begin(); i != row.end(); ++i){ //compute SD
+          var=var+ ((*i-mean) * (*i-mean));
+      }
+    sd=std::sqrt(var/(row_size-1));  
+    int round_off = 10000;
+    float a;
+    for(auto i = row.begin(); i != row.end(); ++i) //compute zscore
+      {
+          a = std::ceil((*i - mean)*round_off/sd) ;
+          *i=a/round_off;
+      }  
+}
+
+//function to compute zscore and replace num matrix with zscores
+// [[Rcpp::export]]
+NumericMatrix compute_write_zscore(NumericMatrix num, int no_threads =1) 
+{
+      std::vector<std::thread*> threads;
+      Rcout<<"Utilizing "<<no_threads<<" threads to compute zscore"<<"\n";
+      for( int i =0; i < num.rows();i+=no_threads){
+          if (i>num.rows()-no_threads) //check for last rows i.e corner case
+          {
+          for (int j=i; j<num.rows();j++)
+          {
+          compute_zscore_row(num( j , _ ));
+        }
+        break;
+      }
+  for (int jj =0; jj < no_threads;++jj) //fire threads
+  {
+    threads.push_back(new std::thread(compute_zscore_row,num(i+jj, _ )));
+  //  Rcout <<i+jj<<"\n";
+  }
+  for (int jj =0; jj < no_threads;++jj) // join threads and clean heap
+  {
+      threads.at(jj) -> join();
+      delete threads.at(jj);
+  }    
+  threads.clear(); //clear the vector
+}
+//NumericMatrix::Row  col = num( 0 , _ );
+//for (auto i = col.begin(); i != col.end(); ++i)
+//     Rcout << *i << "\n";
+
+//write_file(num,"written.tsv"); //write the num matrix to tsv format
+return num; //return matrix with zscores in it
+}
+
+/* keeping it for future
+List compute_zscore_1(List num) {
+int n = num.size();
+float a =0;
+float sum =0;
+float st = 0;
+float var = 0;
+float mean =0;
+for(int i = 0; i < n; ++i) {
+  a=num[i];
+  a = std::log2(a + 1);
+  sum=sum+a;
+  num[i]=a;
+} 
+mean=sum/n;
+for(int i =0; i < n; ++i){
+  a=num[i];
+  var=var+(a-mean)*(a-mean);
+}
+st=std::sqrt(var/(n-1));
+int round_off = 10000;
+for(int i = 0; i < n; ++i) {
+    a=num[i];
+    a = std::ceil((a - mean)*round_off/st) ;
+    num[i]=a/round_off;
+}  
+return num;
+}
+*/
