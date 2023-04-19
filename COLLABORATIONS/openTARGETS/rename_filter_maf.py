57b9b01f28b8d04beeb53a19a344cd1e0b8c3ad4
migbro <miguel.a.brown@gmail.com>
2023-04-19 20:39:45 +0000

    :pencil: created reference header file :tada: added dgd maf processing script :muscle: refactor main filter mad sc ript for openpedcan to reuse functions

--- COLLABORATIONS/openTARGETS/rename_filter_maf.py
+++ COLLABORATIONS/openTARGETS/rename_filter_maf.py
@@ -2,15 +2,6 @@ import sys
 import argparse
 import gzip
 
-parser = argparse.ArgumentParser(description='Script to pre-filter entries on usually removed criteria except TERT promoter, convert BS IDs to cBio names')
-parser.add_argument('-m', '--mapping-file', action='store', dest = 'mapping_file', help='tsv file with header and bs_id, sample type, cbio ID mappings')
-parser.add_argument('-v', '--maf-file', action='store', dest='maf_file',
-                    help='openX maf file')
-parser.add_argument('-s', '--skip', action='store', dest='skip', default=False,
-                    help='Skip typical #version header line')
-parser.add_argument('-n', '--study', action='store', dest='type',
-                    help='study name, like "openpbta"')
-
 
 def populate_id_map(map_fn):
     """
@@ -29,45 +20,61 @@ def populate_id_map(map_fn):
     return id_dict
 
 
-def process_maf_entry(data):
+def process_maf_entry(data, maf_exc, v_idx, h_idx, tid_idx, eid_idx, map_dict):
     datum = data.rstrip('\n').split('\t')
     if datum[tid_idx] in map_dict:
         # Want to allow TERT promoter as exception to exlcusion rules
         if datum[v_idx] not in maf_exc or (datum[h_idx] == "TERT" and datum[v_idx] == "5'Flank"):
             samp_id = map_dict[datum[tid_idx]]
             datum[tid_idx] = samp_id
-            datum.pop(eid_idx)
-            maf_out.write("\t".join(datum) + "\n")
+            if eid_idx != None:
+                datum.pop(eid_idx)
+            return datum
+    else:
+        return None
 
 
-args = parser.parse_args()
-sys.stderr.write("Getting mapping IDs\n")
-map_dict = populate_id_map(args.mapping_file)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script to pre-filter entries on usually removed criteria except TERT promoter, convert BS IDs to cBio names')
+    parser.add_argument('-m', '--mapping-file', action='store', dest = 'mapping_file', help='tsv file with header and bs_id, sample type, cbio ID mappings')
+    parser.add_argument('-v', '--maf-file', action='store', dest='maf_file',
+                        help='openX maf file')
+    parser.add_argument('-s', '--skip', action='store', dest='skip', default=False,
+                        help='Skip typical #version header line')
+    parser.add_argument('-n', '--study', action='store', dest='type',
+                        help='study name, like "openpbta"')
 
-maf_exc = {"Silent": 0, "Intron": 0, "IGR": 0, "3'UTR": 0, "5'UTR": 0, "3'Flank": 0, "5'Flank": 0, "RNA": 0}
 
-# process header, track key indices and drop entrez ID
-maf_file = gzip.open(args.maf_file)
-maf_out = open(args.type + ".maf", "w")
-if args.skip:
+    args = parser.parse_args()
+    sys.stderr.write("Getting mapping IDs\n")
+    map_dict = populate_id_map(args.mapping_file)
+
+    maf_exc = {"Silent": 0, "Intron": 0, "IGR": 0, "3'UTR": 0, "5'UTR": 0, "3'Flank": 0, "5'Flank": 0, "RNA": 0}
+
+    # process header, track key indices and drop entrez ID
+    maf_file = gzip.open(args.maf_file)
+    maf_out = open(args.type + ".maf", "w")
+    if args.skip:
+        head = next(maf_file).decode()
+        maf_out.write(head)
+    else:
+        sys.stderr.write('Skip flag not given. Adding typical #version header for best compatibility\n')
+        print('#version 2.4', file=maf_out)
     head = next(maf_file).decode()
-    maf_out.write(head)
-else:
-    sys.stderr.write('Skip flag not given. Adding typical #version header for best compatibility\n')
-    maf_out.write('#version 2.4\n')
-head = next(maf_file).decode()
-header = head.rstrip("\n").split("\t")
-tid_idx = header.index('Tumor_Sample_Barcode')
-v_idx = header.index('Variant_Classification')
-h_idx = header.index('Hugo_Symbol')
-eid_idx = header.index('Entrez_Gene_Id')
-header.pop(eid_idx)
-maf_out.write("\t".join(header) + "\n")
+    header = head.rstrip("\n").split("\t")
+    tid_idx = header.index('Tumor_Sample_Barcode')
+    v_idx = header.index('Variant_Classification')
+    h_idx = header.index('Hugo_Symbol')
+    eid_idx = header.index('Entrez_Gene_Id')
+    header.pop(eid_idx)
+    print("\t".join(header), file=maf_out)
 
-sys.stderr.write("Filtering entries and renaming samples\n")
+    sys.stderr.write("Filtering entries and renaming samples\n")
 
-for line in maf_file:
-    process_maf_entry(line.decode())
+    for line in maf_file:
+        to_print = process_maf_entry(line.decode(), maf_exc, v_idx, h_idx, tid_idx, eid_idx, map_dict)
+        if to_print:
+            print("\t".join(to_print), file=maf_out)
 
-sys.stderr.write("Fin.\n")
-maf_out.close()
+    sys.stderr.write("Fin.\n")
+    maf_out.close()
