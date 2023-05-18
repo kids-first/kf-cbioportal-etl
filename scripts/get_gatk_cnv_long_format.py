89b41688226144a5a7dcb64b4a940f1bf2e6981b
Saksham Phul <phulsaksham2@gmail.com>
2023-05-18 16:15:34 -0400

    Address duplicate genes and fix the bug

--- scripts/get_gatk_cnv_long_format.py
+++ scripts/get_gatk_cnv_long_format.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import argparse
 from concurrent.futures import ThreadPoolExecutor
-
+import sys
 '''
 command line
 python3 get_gatk_cnv_long_format.py --segfiles_folder_path gatk_copy_ratio_segments_tumor --manifest manifest.tsv
@@ -116,25 +116,46 @@ def prepare_gat_cnv_from_seg_file(seg_file,sample_ID,ref_gene_bed_list):
     seg_data_df=seg_data_df[seg_data_df["gene"].astype(bool)]
     cnv_per_sample=seg_data_df
     cnv_per_sample.rename(columns={"gene":"Hugo_Symbol"},inplace=True)
-    cnv_discrete_per_sample=cnv_per_sample.drop(["cnv_score_continuous"],axis=1)
+
+    df_without_duplicate_gene = cnv_per_sample[cnv_per_sample['Hugo_Symbol'].duplicated(keep=False) == False] # dropping all the duplicate genes
+    df_duplicate_genes = cnv_per_sample[cnv_per_sample['Hugo_Symbol'].duplicated(keep=False) == True] # extracting duplicate genes
+    df2_loss_cnv=df_duplicate_genes[df_duplicate_genes['cnv_score_discrete']<0] #creteria for loss
+    df2_gain_cnv=df_duplicate_genes[df_duplicate_genes['cnv_score_discrete']>0] #creteria for gain
+    df2_loss_cnv = df2_loss_cnv.sort_values('cnv_score_discrete', ascending=True).drop_duplicates('Hugo_Symbol').sort_index() # remove duplicate gene and pick one with min cnv score for loss
+    df2_gain_cnv = df2_gain_cnv.sort_values('cnv_score_discrete', ascending=False).drop_duplicates('Hugo_Symbol').sort_index() # remove duplicate gene and pick one with max cnv score for gain
+    
+    df_without_duplicate_gene=pd.concat([df_without_duplicate_gene,df2_loss_cnv,df2_gain_cnv]) #Add duplicate gene back
+    #print(df_without_duplicate_gene)
+    #print(df2_loss_cnv)
+    #print(df2_gain_cnv)
+    #print(df_without_duplicate_gene)
+    cnv_discrete_per_sample=df_without_duplicate_gene.drop(["cnv_score_continuous"],axis=1) #prepare df for discrete scores
     cnv_discrete_per_sample=cnv_discrete_per_sample.rename(columns={"cnv_score_discrete":str(sample_ID)})
-    cnv_continuous_per_sample=cnv_per_sample.drop(['cnv_score_discrete'],axis=1)
+    cnv_continuous_per_sample=df_without_duplicate_gene.drop(['cnv_score_discrete'],axis=1) #prepare df for continuous scores
     cnv_continuous_per_sample['cnv_score_continuous']=cnv_continuous_per_sample["cnv_score_continuous"].astype('int')
     cnv_continuous_per_sample=cnv_continuous_per_sample.rename(columns={'cnv_score_continuous':str(sample_ID)})
 
     return [cnv_discrete_per_sample,cnv_continuous_per_sample]
 
+def outer_merge_list(df_list):
+    merged_list=df_list[0]
+    for i in range(1,len(df_list),1):
+        merged_list=pd.merge(merged_list,df_list[i],how='outer',on=["Hugo_Symbol"])
+    return merged_list
 
-manifest=pd.read_csv(args.manifest,sep='\t',header=0)
-file_name=manifest['s3_path'].str.split("/",expand=True)[5]
-file_name=file_name.rename("filename")
-map_bs_filename=pd.concat([manifest['biospecimen_id'],file_name],axis=1)
+if not os.path.isfile(args.manifest):
+        raise TypeError("Cannot find given manifest file") 
+else:
+    manifest=pd.read_csv(args.manifest,sep='\t',header=0)
+    file_name=manifest['s3_path'].str.split("/",expand=True)[5]
+    file_name=file_name.rename("filename")
+    map_bs_filename=pd.concat([manifest['biospecimen_id'],file_name],axis=1)
 
+ref_gene_bed_list=get_ref_bed_data('Homo_sapiens.GRCh38.105.chr.gtf_genes.bed')   #used for gene mapping to seg calls 
 
 threads=[]
 results=[]
 counter=0
-ref_gene_bed_list=get_ref_bed_data('Homo_sapiens.GRCh38.105.chr.gtf_genes.bed')
 with ThreadPoolExecutor(max_workers=100) as executor:
     for filename in os.listdir(args.segfiles_folder_path):
         file_path = os.path.join(args.segfiles_folder_path, filename)
@@ -142,25 +163,27 @@ with ThreadPoolExecutor(max_workers=100) as executor:
         if os.path.isfile(file_path) and file_path.endswith(".seg"):
             file_name=file_path.split('/')[1] # file name from split
             sample_ID=map_bs_filename[map_bs_filename["filename"]==file_name]['biospecimen_id'].values[0]
+            file_path='wxs_male_vs_male_delme.tumor.gatk_cnv.called.seg'
             threads.append(executor.submit(prepare_gat_cnv_from_seg_file,file_path,sample_ID,ref_gene_bed_list))
-            #counter=counter+1
-            #if counter ==10:
-            #    break
+            counter=counter+1
+            if counter == 10:
+                break
     for active_threads in threads:
         results.append(active_threads.result())              
 
 # code to split it into 2 lists
-res1, res2 = map(list, zip(*results))
-df_discrete=pd.concat(res1)
+discrete_list, continous_list = map(list, zip(*results))
+df_discrete=outer_merge_list(discrete_list)
 df_discrete=df_discrete.replace(np.nan,'', regex=True)
-df_continous=pd.concat(res2)
+df_continous=outer_merge_list(continous_list)
 df_continous=df_continous.replace(np.nan,'', regex=True)
 df_continous=df_continous.sort_values("Hugo_Symbol")
 df_discrete=df_discrete.sort_values("Hugo_Symbol")
 df_continous.to_csv("cnv_continous.txt",sep='\t',index=False)
 df_discrete.to_csv("cnv_discrete.txt",sep='\t',index=False)
-print(df_discrete)
-print(df_continous)
+#print(df_discrete)
+#print(df_discrete)
+#print(df_continous)
 
 #print(cnv_discrete_per_sample)
 #print(cnv_continuous_per_sample)
