e1566e3c67badde3db80eb73ecd77cf9fac4bc94
Miguel Brown <miguel.a.brown@gmail.com>
2024-09-26 14:38:27 +0000

    :pencil: updated configs and add comments to Bo's perl script to make more clear

--- scripts/get_cbio_copy_only_num.pl
+++ scripts/get_cbio_copy_only_num.pl
@@ -1,4 +1,6 @@
-#! usr/bin/env perl
+#! /usr/bin/env perl
+# Script to output a headerless TSV with hugo gene symbol <tab> entrez ID <tab> copy number for downstrema processing
+# Positional args, file 1 is a Hugo <tab> entrez ID TSV, file 2 is output from cnv_1_genome2gene.py
 use strict;
 use warnings FATAL => 'all';
 use Try::Tiny;
@@ -8,6 +10,7 @@ my $in2=shift;
 my %hash;
 my %cbio;
 
+# First file is hugo tsv, with gene symbols and entrez IDs
 open IN, $in;
 while(<IN>){
 	chomp;
@@ -20,15 +23,18 @@ while(<IN>){
 		$hash{$a[0]} = 0;
 	}
 }
+# Second file is output of bedtools intersect of ControlFreeC p value file filtered by cnv_1_genome2gene.py and a bed file with coords and gene names
 open IN2, $in2;
 while(<IN2>){
 	try{
 		chomp;
 		my @b=split(/\t/,$_);
+		# CN value
 		my $num=$b[3];
+		# ensure gene name is in hugo file
 		if (exists $hash{$b[-1]}){
+			# hashes by gene name <tab> entrez id 
 			my $as="$b[-1]\t$hash{$b[-1]}";
-	#			print "$b[-1]\t$hash{$b[-1]}\t$num\n";
 				$cbio{$as}=$num;
 				}
 		}
