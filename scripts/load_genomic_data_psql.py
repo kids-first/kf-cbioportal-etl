ca1867c96431284e6f3f6d0a59bb947d572c9782
Miguel Brown <miguel.a.brown@gmail.com>
2024-11-14 13:58:27 -0500

    :construction: dev work using pool plus concurrent

--- scripts/load_genomic_data_psql.py
+++ scripts/load_genomic_data_psql.py
@@ -6,10 +6,10 @@ YOU MUST CREATE YOUR OWN .ini FILE WITH LOGIN PARAMS BEFORE RUNNING
 """
 import psycopg
 from psycopg import sql
-from psycopg_pool import AsyncConnectionPool
+from psycopg_pool import ConnectionPool
+import concurrent.futures
 from get_study_metadata import config
 import argparse
-import asyncio
 import pdb
 import sys
 
@@ -28,11 +28,11 @@ def format_tbl_name(tbl_path):
     return formatted_tpath
 
 
-async def mt_execute_data_load(line, formatted_tpath, header, db_conn):
+def mt_execute_data_load(line, formatted_tpath, header, db_conn):
     try:
         info = line.rstrip('\n').split('\t')
         vals = [float(old) for old in info[1:]]
-        await db_conn.execute(sql.SQL("INSERT INTO {name} ({primary_key}, samples, data) VALUES (%s, %s, %s)").format(name=formatted_tpath, primary_key=sql.Identifier(header[0])), [info[0], header[1:], vals])
+        db_conn.execute(sql.SQL("INSERT INTO {name} ({primary_key}, samples, data) VALUES (%s, %s, %s)").format(name=formatted_tpath, primary_key=sql.Identifier(header[0])), [info[0], header[1:], vals])
     except psycopg.Error as e:
         print(e, file=sys.stderr)
         print("Failed data load for primary key {}".format(info[0]), file=sys.stderr)
@@ -41,7 +41,7 @@ async def mt_execute_data_load(line, formatted_tpath, header, db_conn):
         exit(1)
 
 
-async def create_table(header, data, tbl_path, data_type, formats, db_pool, wtypes):
+def create_table(header, data, tbl_path, data_type, formats, db_pool, wtypes):
     """
     This function creates the desired table from a formatted cBio file depending on it's table format:
     wide (gene by sample name) or long (tsv with headers) then loads accordingly 
@@ -50,26 +50,33 @@ async def create_table(header, data, tbl_path, data_type, formats, db_pool, wtyp
         print("Creating {} type table for {} type data, starting with table def".format(formats[data_type], data_type), file=sys.stderr)
         (formatted_tpath) = format_tbl_name(tbl_path)
         print("DEBUG: Waiting for pool connection", file=sys.stderr)
-        await db_pool.open()
+        db_pool.open(wait=True)
         create_sql = sql.SQL("CREATE TABLE IF NOT EXISTS {name} ({primary_key} PRIMARY KEY, samples varchar[], {data})").format(name=formatted_tpath, primary_key=sql.SQL(" {} {}").format(sql.Identifier(header[0]), sql.SQL("varchar")),data=sql.SQL(" {} {}").format(sql.Identifier("data"), sql.SQL(wtypes[data_type])))
-        async with db_pool.connection() as db_conn:
-            await db_conn.execute(create_sql)
-            await db_conn.commit()
+        with db_pool.connection() as db_conn:
+            db_conn.execute(create_sql)
+            # db_conn.commit()
             print("Loading data from TSV", file=sys.stderr)
             x = 1
             m = 500
-
-            for line in data:
-                await mt_execute_data_load(line, formatted_tpath, header, db_conn)
-                if x % m == 0:
-                    print("Processed {} entries to commit".format(x), file=sys.stderr)
-                    sys.stderr.flush()
-                    await db_conn.commit()
-                x += 1
-            if x % m != 0:
-                await db_conn.commit()
-        await db_pool.wait()
-        await db_pool.close()
+            with concurrent.futures.ThreadPoolExecutor(4) as executor:
+                results = {
+                    executor.submit(mt_execute_data_load, line, formatted_tpath, header, db_conn): line
+                    for line in data
+                }
+                for result in concurrent.futures.as_completed(results):
+                    if x % m == 0:
+                        print("Processed {} entries to commit".format(x), file=sys.stderr)
+                        sys.stderr.flush()
+                    x += 1
+            # for line in data:
+            #     mt_execute_data_load(line, formatted_tpath, header, db_conn)
+            #     if x % m == 0:
+            #         print("Processed {} entries to commit".format(x), file=sys.stderr)
+            #         sys.stderr.flush()
+            #     x += 1
+            # db_conn.commit()
+            # db_pool.wait()
+            # db_pool.close()
 
         # return formatted_tpath, header
 
@@ -114,7 +121,7 @@ def main():
     parser.add_argument("-n", "--table-name", action="store", dest="name", help="Name of table, including schema")
     parser.add_argument("-i", "--input", action="store", dest="input", help="Input tsv file")
     parser.add_argument("-t", "--type", action="store", dest="type", help="cBio file type, i.e. cnv, maf, rsem, sv")
-    parser.add_argument("-s", "--size", action="store_true", dest="size", help="Max size for pool", default=12, type=int)
+    parser.add_argument("-s", "--max-size", action="store", dest="size", help="Max size for pool", type=int, default=12)
 
     args = parser.parse_args()
 
@@ -129,10 +136,11 @@ def main():
         db = config(filename=args.db_ini, section=args.profile)
         psql_uri = "postgresql://{user}:{password}@{host}/{dbname}".format(user=db['user'], password=db['password'], host=db['host'], dbname=db['database'])
         print("DEBUG: Establishing pool connection", file=sys.stderr)
-        pool = AsyncConnectionPool(conninfo=psql_uri, open=False, max_size=args.size)
+        kwargs = { "autocommit" :True }
+        pool = ConnectionPool(conninfo=psql_uri, kwargs=kwargs, open=False, max_size=args.size)
 
         if args.mode == 'create':
-            asyncio.run(create_table(header, file_in, args.name, args.type, formats, pool, wtypes))
+            create_table(header, file_in, args.name, args.type, formats, pool, wtypes)
             print("Table {} created!".format(args.name))
         elif args.mode == 'append':
             # append_table(header, file_in, args.name, args.type, formats, cur, conn, wtypes)
