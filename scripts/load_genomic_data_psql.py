41dfd2dc6ed7c448f1f01fbb4f5dd57ffb4889bc
Miguel Brown <miguel.a.brown@gmail.com>
2024-11-15 16:59:21 +0000

    :hammer: have a suitable load speed for wide format. append is next

--- scripts/load_genomic_data_psql.py
+++ scripts/load_genomic_data_psql.py
@@ -5,10 +5,7 @@ You can either initialize a table using "create" mode, add data/samples using "a
 YOU MUST CREATE YOUR OWN .ini FILE WITH LOGIN PARAMS BEFORE RUNNING
 """
 import psycopg
-import psycopg2.extras
 from psycopg import sql
-from psycopg_pool import ConnectionPool
-import concurrent.futures
 from get_study_metadata import config
 import argparse
 import pdb
@@ -44,7 +41,7 @@ def mt_execute_data_load(line, formatted_tpath, header, db_conn):
         return 1
 
 
-def create_table(header, data, tbl_path, data_type, formats, db_pool, wtypes):
+def create_table(header, data, tbl_path, data_type, formats, db_conn, wtypes):
     """
     This function creates the desired table from a formatted cBio file depending on it's table format:
     wide (gene by sample name) or long (tsv with headers) then loads accordingly 
@@ -53,22 +50,25 @@ def create_table(header, data, tbl_path, data_type, formats, db_pool, wtypes):
         print("Creating {} type table for {} type data, starting with table def".format(formats[data_type], data_type), file=sys.stderr)
         (formatted_tpath, tname) = format_tbl_name(tbl_path)
         print("DEBUG: Waiting for pool connection", file=sys.stderr)
-        db_pool.open(wait=True)
         create_sql = sql.SQL("CREATE TABLE IF NOT EXISTS {name} ({primary_key} PRIMARY KEY, samples varchar[], {data})").format(name=formatted_tpath, primary_key=sql.SQL(" {} {}").format(sql.Identifier(header[0]), sql.SQL("varchar")),data=sql.SQL(" {} {}").format(sql.Identifier("data"), sql.SQL(wtypes[data_type])))
-        with db_pool.connection() as db_conn:
-            db_conn.execute(create_sql)
-            # db_conn.commit()
-            print("Loading data from TSV", file=sys.stderr)
-            x = 1
-            m = 1000
-
-            batch_load = []
-            for line in data:
-                info = line.rstrip('\n').split('\t')
-                batch_load.append( (info[0], header[1:], [float(old) for old in info[1:]] ) )
-            print("Batch load into table", file=sys.stderr)
-            load_sql = f"""INSERT INTO {tname} VALUES (%s, %s, %s);"""
+        db_conn.execute(create_sql)
+        num_records = 1
+        batch_size = 2000
+        print("Loading data from TSV in batches of {} records".format(batch_size), file=sys.stderr)
+        batch_load = []
+        load_sql = f"""INSERT INTO {tname} VALUES (%s, %s, %s);"""
+        for line in data:
+            if num_records % batch_size == 0:
+                print("Read {} records into memory".format(batch_size))
+                db_conn.cursor().executemany(load_sql, batch_load)
+                print("Loaded {} records into table".format(num_records))
+                batch_load = []
+            info = line.rstrip('\n').split('\t')
+            batch_load.append( (info[0], header[1:], [float(old) for old in info[1:]] ) )
+            num_records += 1
+        if num_records % batch_size != 0:
             db_conn.cursor().executemany(load_sql, batch_load)
+        print("Batch load into table completed!", file=sys.stderr)
 
 
 def main():
@@ -80,7 +80,6 @@ def main():
     parser.add_argument("-n", "--table-name", action="store", dest="name", help="Name of table, including schema")
     parser.add_argument("-i", "--input", action="store", dest="input", help="Input tsv file")
     parser.add_argument("-t", "--type", action="store", dest="type", help="cBio file type, i.e. cnv, maf, rsem, sv")
-    parser.add_argument("-s", "--max-size", action="store", dest="size", help="Max size for pool", type=int, default=12)
 
     args = parser.parse_args()
 
@@ -94,19 +93,17 @@ def main():
         # Load database login info
         db = config(filename=args.db_ini, section=args.profile)
         psql_uri = "postgresql://{user}:{password}@{host}/{dbname}".format(user=db['user'], password=db['password'], host=db['host'], dbname=db['database'])
-        print("DEBUG: Establishing pool connection", file=sys.stderr)
-        kwargs = { "autocommit" :True }
-        pool = ConnectionPool(conninfo=psql_uri, kwargs=kwargs, open=False, max_size=args.size)
-
-        if args.mode == 'create':
-            create_table(header, file_in, args.name, args.type, formats, pool, wtypes)
-            print("Table {} created!".format(args.name))
-        elif args.mode == 'append':
-            # append_table(header, file_in, args.name, args.type, formats, cur, conn, wtypes)
-            print("Append to {} completed!".format(args.name))
-        else:
-            print("ERROR, mode choices are create or append, you chose {mode}".format(mode=args.mode))
-            exit(1)
+        print("Establishing connection", file=sys.stderr)
+        with psycopg.connect(conninfo=psql_uri, autocommit=True) as db_conn:
+            if args.mode == 'create':
+                create_table(header, file_in, args.name, args.type, formats, db_conn, wtypes)
+                print("Table {} created!".format(args.name))
+            elif args.mode == 'append':
+                # append_table(header, file_in, args.name, args.type, formats, cur, conn, wtypes)
+                print("Append to {} completed!".format(args.name))
+            else:
+                print("ERROR, mode choices are create or append, you chose {mode}".format(mode=args.mode))
+                exit(1)
 
 if __name__ == "__main__":
     main()
