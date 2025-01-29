3de9f636ea5460228dd8c6a52294cae92422daad
Miguel Brown <miguel.a.brown@gmail.com>
2025-01-29 16:14:32 -0500

    :hammer: most of logic worked out to compare timeline data

--- cbioportal_etl/scripts/diff_studies.py
+++ cbioportal_etl/scripts/diff_studies.py
@@ -6,6 +6,7 @@ Script to check a study on pedcbioportal for differences against a local build
 import argparse
 from urllib.parse import urlparse
 import requests
+import glob
 import pdb
 
 
@@ -89,16 +90,6 @@ def table_to_dict(in_file, key, aggr_list):
     return data_dict, attr_set
 
 
-def data_clinical_treatment_from_study(url, token, study_id):
-    """
-    Get and compare treatment data when available
-    """
-    headers = { "Authorization": f"Bearer {token}"}
-    api_url = f"{url}api/studies/{study_id}/clinical-events?projection=DETAILED"
-    study_timeline_data = requests.get(api_url, headers=headers, timeout=360).json()
-    return study_timeline_data
-
-
 def data_clinical_from_study(url, auth_headers, study_id, data_type, aggr_list):
     """
     Get all the column-value pairs for each data_type(SAMPLE or PATIENT) for a specific study
@@ -133,6 +124,61 @@ def data_clinical_from_study(url, auth_headers, study_id, data_type, aggr_list):
     return data_dict
 
 
+def data_clinical_treatment_from_study(url, auth_headers, study_id):
+    """
+    Get and compare treatment data when available
+    """
+    # from https://realpython.com/python-data-structures/#dataclassesdataclass-python-37-data-classes
+    common_fields = ["patientId", "startNumberOfDaysSinceDiagnosis", "endNumberOfDaysSinceDiagnosis", "eventType"]
+    portal_timeline_attr_dict = {
+        "Clinical Status": ["CLINICAL_EVENT_TYPE"],
+        "IMAGING": ["DIAGNOSTIC_TYPE", "DIAGNOSTIC_TYPE_DETAILED", "BODY_PART", "FLYWHEEL_URL"],
+        "SPECIMEN": ["SAMPLE_ID", "SPECIMEN_SITE", "SPECIMEN_TYPE"],
+        "SURGERY": ["EVENT_TYPE", "SUBTYPE", "EXTENT_OF_TUMOR_RESECTION"],
+        "TREATMENT": ["TREATMENT_TYPE", "SUBTYPE", "AGENT"]
+    }
+
+    clinical_events_study_path = f"api/studies/{study_id}/clinical-events?projection=DETAILED"
+    study_timeline_data = requests.get(f"{url}/{clinical_events_study_path}", headers=auth_headers, timeout=360).json()
+    portal_timeline_data_dict = {}
+    for attr in portal_timeline_attr_dict:
+        portal_timeline_data_dict[attr] = []
+    for entry in study_timeline_data:
+        event_type = entry['eventType']
+        temp_event_list = []
+        for common in common_fields:
+            if common in entry:
+                temp_event_list.append(entry[common])
+            else:
+                temp_event_list.append("")
+        event_specific = portal_timeline_attr_dict[event_type]
+        # event-specific attributes have funky organization,like 'attributes': [{'key': 'CLINICAL_EVENT_TYPE', 'value': 'Initial CNS Tumor'}], easier to flatten it into a normal dict: 
+        flatten_attrs = {k: v for d in [ {x['key']: x['value']} for x in entry['attributes'] ] for k, v in d.items()}
+        for field in event_specific:
+            if field in flatten_attrs:
+                temp_event_list.append(flatten_attrs[field])
+            else:
+                temp_event_list.append("")
+        portal_timeline_data_dict[event_type].append("\t".join(map(str, temp_event_list)))
+
+
+    return portal_timeline_data_dict
+
+
+def data_clinical_timeline_file_to_dict(file_path):
+    with open(file_path) as flat_file:
+        data_list = flat_file.read().splitlines()
+    return data_list
+
+
+def compare_timeline_data(portal_timeline, load_timeline):
+    event_type = portal_timeline.keys()
+    for event in event_type:
+        portal_set = set(portal_timeline[event])
+        load_set = set(load_timeline[event])
+        print(load_set - portal_set)
+
+
 def run_py(args):
     with open(args.token, 'r') as token_file:
         token = token_file.read().rstrip().split(': ')[1]
@@ -154,7 +200,7 @@ def run_py(args):
     portal_patient_attr_keys = set([x['clinicalAttributeId'] for x in attr_key_json if x['patientAttribute']])
 
     # gather sample-level metadata
-    portal_sample_data = data_clinical_from_study(formatted_url, auth_headers, args.study, "SAMPLE", aggr_list)
+    portal_sample_data = data_clinical_from_study(url=formatted_url, auth_headers=auth_headers, study_id=args.study, data_type="SAMPLE", aggr_list=aggr_list)
     build_sample_data, build_sample_attr_keys = table_to_dict(args.datasheet_sample, "SAMPLE_ID", aggr_list)
     # implicit attributes not returned by function that are required for sample view
     portal_sample_attr_keys.update(portal_sample_attr_implicit)
@@ -164,11 +210,27 @@ def run_py(args):
     with open('sample_portal_v_build.txt', 'w') as sample_diff_out:
         clinical_diffs(portal_sample_data, build_sample_data, portal_sample_attr_keys, build_sample_attr_keys, "Sample", sample_diff_out)
     # patient-level diffs
-    portal_patient_data = data_clinical_from_study(formatted_url, auth_headers, args.study, "PATIENT", aggr_list)
+    portal_patient_data = data_clinical_from_study(url=formatted_url, auth_headers=auth_headers, study_id=args.study, data_type="PATIENT", aggr_list=aggr_list)
     build_patient_data, build_patient_attr_keys = table_to_dict(args.datasheet_patient, "PATIENT_ID", aggr_list)
     # timeline-diffs
-    # if args.dt:
-    #     data_clinical_treatment_from_study(url=[url_object.scheme, url_object.hostname].join("//"), token=token, study_id=args.study)
+    if args.datasheet_timeline:
+        timeline_event_dict = {
+            "clinical_event.txt": "Clinical Status",
+            "imaging.txt": "IMAGING",
+            "specimen.txt": "SPECIMEN",
+            "surgery.txt": "SURGERY",
+            "treatment.txt": "TREATMENT", 
+        }
+        timeline_portal = data_clinical_treatment_from_study(url=formatted_url, auth_headers=auth_headers, study_id=args.study)
+        timeline_dir = args.datasheet_timeline.rstrip("/")
+        timeline_flatfile_prefix = f"{timeline_dir}/data_clinical_timeline_"
+        timeline_flatfile_list = glob.glob(f"{timeline_flatfile_prefix}*")
+        timeline_flatfile_dict = {}
+        for path in timeline_flatfile_list:
+            suffix = path.replace(timeline_flatfile_prefix, "")
+            timeline_flatfile_dict[timeline_event_dict[suffix]] = data_clinical_timeline_file_to_dict(path)
+        compare_timeline_data(portal_timeline=timeline_portal, load_timeline=timeline_flatfile_dict)
+
     # drop attributes that are post-load portal-specific
     portal_patient_attr_keys -= set(portal_patient_attr_skip)
     with open('patient_portal_v_build.txt', 'w') as patient_diff_out:
