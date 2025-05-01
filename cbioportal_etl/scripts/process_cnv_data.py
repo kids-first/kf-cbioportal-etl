06cf4b9c484a58ed3b9e53343d09edb5a313b5ea
Miguel Brown <miguel.a.brown@gmail.com>
2025-05-01 21:11:46 +0000

    :construction: mostly worked out GATK, need to resolve CN 2 vs GATK + call

--- cbioportal_etl/scripts/process_cnv_data.py
+++ cbioportal_etl/scripts/process_cnv_data.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 """Read in GATK and ControlFreeC CNV data and convert to cBio format."""
 
-from curses import raw
 import sys
 import pdb
 import csv
 from pybedtools import BedTool
-
 
 
 def get_gene_cnv_dict(cnv_bed_obj: BedTool, ref_bed: BedTool, ploidy: int = 2, high_gain: int = 4) -> tuple[dict[str, int], dict[str, int]]:
@@ -45,12 +43,12 @@ def get_gene_cnv_dict(cnv_bed_obj: BedTool, ref_bed: BedTool, ploidy: int = 2, h
 def read_and_process_ctrlfreec_pval(pval_fname: str, ref_bed: BedTool, ploidy: int = 2) -> None:
     PVALUE: float = 0.05
     with open(pval_fname) as f:
-        cnv_list: list[list[str]] = list(csv.reader(f, delimiter="\t"))
-        header: list[str] = cnv_list[0]
+        cnv_reader: csv.reader._reader = csv.reader(f, delimiter="\t")
+        header = next(cnv_reader)
         wilcox_idx: int = header.index("WilcoxonRankSumTestPvalue")
         ks_idx: int = header.index("KolmogorovSmirnovPvalue")
         # create bed str representation of CNV data, filtering by pvalue
-        cnv_filtered_as_bed: str = "\n".join(["\t".join(bed[0:4]) for bed in cnv_list[1:] if float(bed[wilcox_idx]) <= PVALUE and float(bed[ks_idx]) <= PVALUE])
+        cnv_filtered_as_bed: str = "\n".join(["\t".join(bed[0:4]) for bed in cnv_reader if float(bed[wilcox_idx]) <= PVALUE and float(bed[ks_idx]) <= PVALUE])
         cnv_bed_obj: BedTool = BedTool(cnv_filtered_as_bed, from_string=True)
         raw_cnv_dict, gistic_cnv_dict = get_gene_cnv_dict(cnv_bed_obj, ref_bed, ploidy)
         with open("test_raw_cnv.txt", "w") as raw_out, open("test_gistic_cnv.txt", "w") as gistic_out:
@@ -59,12 +57,33 @@ def read_and_process_ctrlfreec_pval(pval_fname: str, ref_bed: BedTool, ploidy: i
                 print(f"{gene}\t{gistic_cnv_dict[gene]}", file=gistic_out)
 
 
-def read_and_process_gatk_cnv():
-    pass
+def read_and_process_gatk_cnv(gatk_seg_fname: str, ref_bed: BedTool):
+    with open(gatk_seg_fname) as f:
+        cnv_reader: csv.reader._reader = csv.reader(f, delimiter="\t")
+        # Need to skip ridiculous interval list-style headers
+        cnv_list = [entry for entry in cnv_reader if not entry[0].startswith("@")]
+        # Drop chrM, convert MEAN_LOG2_COPY_RATIO to CN and rm leading chr
+        cnv_as_bed = ""
+        for entry in cnv_list[1:]:
+            if entry[0] != "chrM":
+                entry[0] = entry[0][3:]
+                # ratio is typically log2(cn/ploidy)
+                delme = entry[-2]
+                entry[-2] = pow(2, float(entry[-2])) * 2
+                # delme sanity check
+                if round(entry[-2]) == 2 and entry[5] == "+":
+                    print (*[*entry, delme], sep="\t")
+                cnv_as_bed += f"{entry[0]}\t{entry[1]}\t{entry[2]}\t{entry[-2]}\n"
+        cnv_bed_obj: BedTool = BedTool(cnv_as_bed, from_string=True)
+        raw_cnv_dict, gistic_cnv_dict = get_gene_cnv_dict(cnv_bed_obj, ref_bed)
+        with open("gatk_raw_cnv.txt", "w") as raw_out, open("gatk_gistic_cnv.txt", "w") as gistic_out:
+            for gene in raw_cnv_dict:
+                print(f"{gene}\t{raw_cnv_dict[gene]}", file=raw_out)
+                print(f"{gene}\t{gistic_cnv_dict[gene]}", file=gistic_out)
 
 def main():
     ref_bed = BedTool(sys.argv[2])
-    read_and_process_ctrlfreec_pval(sys.argv[1], ref_bed)
-
+    # read_and_process_ctrlfreec_pval(sys.argv[1], ref_bed)
+    read_and_process_gatk_cnv(sys.argv[1], ref_bed)
 if __name__ == "__main__":
     main()
