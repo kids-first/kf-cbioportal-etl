e7dce74804c8ff808b20f2d9db4d713bb909925c
Miguel Brown <miguel.a.brown@gmail.com>
2026-09-03 14:06:57 -0400

    Apply batched suggestions from code review
    Co-authored-by: Daniel Miller <dmiller15@users.noreply.github.com>

--- cbioportal_etl/scripts/rna_merge_isoform_expression.py
+++ cbioportal_etl/scripts/rna_merge_isoform_expression.py
@@ -22,7 +22,7 @@ from scipy import stats
 from cbioportal_etl.scripts.resolve_config_paths import resolve_config_paths
 
 
-def load_rsem_file(rsem_file: str, sample: str, rsem_dir: str, expr_type: str) -> pd.DataFrame:
+def load_rsem_file(rsem_file: str, sample: str, rsem_dir: str, expr_type: str) -> pd.DataFrame | None:
     """Read and format a single RSEM file.
 
     Args:
@@ -43,6 +43,7 @@ def load_rsem_file(rsem_file: str, sample: str, rsem_dir: str, expr_type: str) -
 
 
 if __name__ == "__main__":
+  main()
     parser = argparse.ArgumentParser(description="Merge rsem isoform files using manifest file info.")
     parser.add_argument(
         "-t",
