3ff32be3154b29e05b6eea3281ff630a36d430f3
Miguel Brown <miguel.a.brown@gmail.com>
2026-09-14 20:36:03 +0000

    :bug: nearly there, but output has bugs

--- cbioportal_etl/scripts/add_germline_to_somatic_maf.py
+++ cbioportal_etl/scripts/add_germline_to_somatic_maf.py
@@ -103,19 +103,12 @@ def main() -> None:
     parser = argparse.ArgumentParser(
         description="Clean up and append germline MAF to somatic MAF for cBioPortal import"
     )
-    parser.add_argument(
-        "-t",
-        "--table",
-        action="store",
-        help="Table with cbio project, kf bs ids, cbio IDs, and file names",
-    )
 
     parser.add_argument(
-        "-j",
-        "--config",
+        "-a",
+        "--append",
         action="store",
-        dest="config_file",
-        help="json config file with data types and data locations",
+        help="MAF to append to",
     )
 
     parser.add_argument(
@@ -127,12 +120,34 @@ def main() -> None:
 
     args = parser.parse_args()
     TOOL_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    germ_data_dict: dict[str, list] = {}
-    with open(args.exported) as f:
+
+    # Build a map of cBio IDs to assign to matched normal, then append
+    matched_ids: dict[str, set] = {}
+    # also get header entries to use for output, if not in germline MAF, will output blank
+    h_dict: dict[str, int|None] = {}
+
+    with open(args.append) as in_maf:
+        _version_line = in_maf.readline()
+        maf_reader: csv.reader._reader = csv.reader(in_maf, delimiter="\t")
+        maf_header = next(maf_reader)
+        maf_tum_id_idx = maf_header.index("Tumor_Sample_Barcode")
+        maf_norm_id_idx = maf_header.index("Matched_Norm_Sample_Barcode")
+        for row in maf_reader:
+            tum_id, norm_id = row[maf_tum_id_idx], row[maf_norm_id_idx]
+            if norm_id not in matched_ids:
+                matched_ids[norm_id] = set()
+            matched_ids[norm_id].add(tum_id)
+
+    with open(args.append, "a") as in_maf, open(args.exported) as f:
         germline_reader: csv.reader._reader = csv.reader(f, delimiter="\t")
+
         header = next(germline_reader)
         header.extend(NEW_FIELDS)
-        print("\t".join(header))
+        for item in maf_header:
+            if item in header:
+                h_dict[item] = header.index(item)
+            else:
+                h_dict[item] = None
         # get indices of fields used to inform fields to be calculated
         sample_id_idx = header.index("Matched_Norm_Sample_Barcode")
         ref_idx = header.index("Reference_Allele")
@@ -145,18 +160,25 @@ def main() -> None:
             data.append(append_amino_acids_field(data[hgvs_p_idx]))
             data.append(var_type)
             data.append(append_variant_class(data[csq_idx], var_type, inframe))
-            # add to dict by sample id
-            sample_id = data[sample_id_idx]
-            if sample_id not in germ_data_dict:
-                germ_data_dict[sample_id] = []
-            germ_data_dict[sample_id].append(data)
-            # debug, print out data
-            print("\t".join(data))
-
-    # with open(args.config_file) as f:
-    #     config_data = json.load(f)
-    # config_data: dict = resolve_config_paths(config_data, TOOL_DIR)
-
+            # now append all matched tumor IDs for this normal to the MAF, fill in fields not in maf_header
+            # repeat for each tumor for now
+            if data[sample_id_idx] in matched_ids:
+                for tum_id in matched_ids[data[sample_id_idx]]:
+                    data_reorg = []
+                    for field, idx in h_dict.items():
+                        if field == "Hugo_Symbol" and data[header.index("Hugo_Symbol")] == "":
+                            print(f"Warning: Hugo_Symbol is empty for {data} in germline MAF", file=sys.stderr)
+                        elif idx is not None:
+                            data_reorg.append(data[idx])
+                        elif field == "Tumor_Sample_Barcode":
+                            data_reorg.append(tum_id)
+                        elif field == "Matched_Norm_Sample_Barcode":
+                            data_reorg.append(data[sample_id_idx])
+                        elif field == "Tumor_Seq_Allele1":
+                            data_reorg.append(data[alt_idx])
+                        else:
+                            data_reorg.append("")
+                    in_maf.write("\t".join(data_reorg) + "\n")
 
 if __name__ == "__main__":
     # set global vars here, then call main
